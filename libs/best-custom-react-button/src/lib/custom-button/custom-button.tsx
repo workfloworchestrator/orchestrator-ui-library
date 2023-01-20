84f6598c2dab0b9bfadf5f7e4b276effe1dbdcbd
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-01-20 13:16:37 +0100

    Setup Emotion config

--- libs/best-custom-react-button/src/lib/custom-button/custom-button.tsx
+++ libs/best-custom-react-button/src/lib/custom-button/custom-button.tsx
@@ -1,15 +1,26 @@
-import styles from './custom-button.module.scss';
-import {FC} from 'react';
+import { FC } from 'react';
 import { EuiButton } from '@elastic/eui';
+import { css } from '@emotion/react';
+
+const contentStyles = css`
+    height: 300px;
+    width: 300px;
+    background-color: aqua;
+`;
+
+const buttonStyles = css`
+    background-color: red;
+`;
 
 /* eslint-disable-next-line */
 export interface CustomButtonProps {
-  buttonText: string
+    buttonText: string;
 }
 
-export const CustomButton:FC<CustomButtonProps> = (props: CustomButtonProps) => {
-  return (
-    <EuiButton fill>Lokale aanpassing</EuiButton>
-  );
-}
-
+export const CustomButton: FC<CustomButtonProps> = ({ buttonText }) => (
+    <div css={contentStyles}>
+        <EuiButton css={buttonStyles} fill>
+            {buttonText}
+        </EuiButton>
+    </div>
+);
