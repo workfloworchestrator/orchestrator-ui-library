7ec3915ea654c9090997038359d3a22b36279314
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-02-16 13:37:04 +0100

    Adds top navigation and sidebar placeholders in the app template

--- apps/example-orchestrator-ui/pages/_app.tsx
+++ apps/example-orchestrator-ui/pages/_app.tsx
@@ -1,17 +1,57 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import React from 'react';
-import { EuiProvider } from '@elastic/eui';
+import React, { useState } from 'react';
+import { EuiProvider, EuiPageTemplate, EuiButton } from '@elastic/eui';
 import { defaultOrchestratorTheme } from '@orchestrator-ui/orchestrator-ui-components';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+    const [sideMenuIsVisible, setSideMenuIsVisible] = useState(true);
+
+    const navigationHeight = 50;
+
     return (
         <EuiProvider colorMode="light" modify={defaultOrchestratorTheme}>
             <Head>
                 <title>Welcome to example-orchestrator-ui!</title>
             </Head>
             <main className="app">
-                <Component {...pageProps} />
+                <>
+                    <div
+                        style={{
+                            backgroundColor: 'blue',
+                            color: 'white',
+                            height: navigationHeight,
+                        }}
+                    >
+                        <span>Navigation</span>
+                        <EuiButton
+                            onClick={() =>
+                                setSideMenuIsVisible(!sideMenuIsVisible)
+                            }
+                        >
+                            Toggle SideMenu
+                        </EuiButton>
+                    </div>
+                </>
+                <EuiPageTemplate
+                    offset={0}
+                    grow={false}
+                    contentBorder={false}
+                    minHeight={`calc(100vh - ${navigationHeight}px)`}
+                >
+                    {sideMenuIsVisible && (
+                        <EuiPageTemplate.Sidebar
+                            style={{ backgroundColor: 'red' }}
+                        >
+                            <div>Cool</div>
+                        </EuiPageTemplate.Sidebar>
+                    )}
+                    <EuiPageTemplate.Section
+                        style={{ backgroundColor: 'grey' }}
+                    >
+                        <Component {...pageProps} />
+                    </EuiPageTemplate.Section>
+                </EuiPageTemplate>
             </main>
         </EuiProvider>
     );
