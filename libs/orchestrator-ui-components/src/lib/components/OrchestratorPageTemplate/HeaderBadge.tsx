fcc46701c4165cedcf9653a1d2d715377043fa73
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-04-12 15:47:57 +0200

    EUI version update to v77.1.0, removes workaround for EuiBadge from project

--- libs/orchestrator-ui-components/src/lib/components/OrchestratorPageTemplate/HeaderBadge.tsx
+++ libs/orchestrator-ui-components/src/lib/components/OrchestratorPageTemplate/HeaderBadge.tsx
@@ -1,30 +1,15 @@
 import React, { FC, ReactNode } from 'react';
 import { EuiBadge } from '@elastic/eui';
-import { EuiBadgeProps, _EuiThemeColorsMode } from '@elastic/eui';
-import { useOrchestratorTheme } from '../../hooks/useOrchestratorTheme';
+import { EuiBadgeProps } from '@elastic/eui';
 
 export type HeaderBadgeProps = EuiBadgeProps & {
     children: ReactNode;
-} & HeaderBadgeColor;
-
-export type HeaderBadgeColor =
-    | {
-          color: keyof _EuiThemeColorsMode;
-          customColor?: false;
-      }
-    | {
-          color: string;
-          customColor: true;
-      };
+};
 
 export const HeaderBadge: FC<HeaderBadgeProps> = ({
-    customColor,
-    color,
     children,
     ...restProps
 }) => {
-    const { theme } = useOrchestratorTheme();
-
     const css = restProps.iconType
         ? {
               height: 24,
@@ -34,11 +19,7 @@ export const HeaderBadge: FC<HeaderBadgeProps> = ({
         : { height: 24, display: 'flex' };
 
     return (
-        <EuiBadge
-            {...restProps}
-            color={!customColor ? theme.colors[color] : color}
-            css={css}
-        >
+        <EuiBadge {...restProps} css={css}>
             {children}
         </EuiBadge>
     );
