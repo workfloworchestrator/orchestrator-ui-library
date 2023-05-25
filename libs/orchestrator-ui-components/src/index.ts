618de5ecee5ea301e30e2c53319a29de3425d8d9
Tjeerd.Verschragen <tjeerd.verschragen@surf.nl>
2023-05-25 14:59:59 +0200

    Change lib exporting with index.ts in all folders

--- libs/orchestrator-ui-components/src/index.ts
+++ libs/orchestrator-ui-components/src/index.ts
@@ -1,22 +1,12 @@
-export * from './lib/components/custom-button/custom-button';
-export * from './lib/components/OrchestratorPageTemplate/OrchestratorPageTemplate';
-export * from './lib/components/Table/Table';
-export * from './lib/components/Table/columns';
-export * from './lib/components/Badges';
+export * from './lib/components';
 
 export * from './lib/icons';
 
-export * from './lib/theme/defaultOrchestratorTheme';
+export * from './lib/theme';
 
 export * from './lib/utils';
 
-export * from './lib/contexts/OrchestratorConfigContext';
-
-export * from './lib/components/StartPage/ListItemStartPage';
-export * from './lib/components/StartPage/ListStartPage';
-export * from './lib/components/StartPage/MultiListSection';
-export * from './lib/components/StartPage/NewProcessPanel';
-export * from './lib/components/StartPage/StatCards';
+export * from './lib/contexts';
 
 export * from './lib/types';
 export * from './lib/hooks';
