9f0b6a390e660d98570b108b112df07df6a787cb
René Dohmen <acidjunk@gmail.com>
2023-05-26 21:25:55 +0200

    Reverted subscription context

--- libs/orchestrator-ui-components/src/lib/components/Subscription/SubscriptionGeneral.tsx
+++ libs/orchestrator-ui-components/src/lib/components/Subscription/SubscriptionGeneral.tsx
@@ -1,53 +1,45 @@
-import React, { FC } from 'react';
-
+import React from 'react';
+import {
+    FixedInputBlock,
+    SubscriptionBlock,
+    SubscriptionContext,
+} from '@orchestrator-ui/orchestrator-ui-components';
 import {
     EuiFlexGrid,
     EuiFlexItem,
     EuiLoadingContent,
     EuiSpacer,
 } from '@elastic/eui';
-import { SubscriptionBlockBase } from '../../types';
-import { SubscriptionBlock } from './SubscriptionBlock';
-import { SubscriptionActionsProps } from './SubscriptionActions';
-// import {FixedInputBlock} from "./FixedInputBlock";
 
-export type SubscriptionGeneralProps = {
-    subscriptionBlock: SubscriptionBlockBase | null;
-};
+export const SubscriptionGeneral = () => {
+    const { subscriptionData, loadingStatus } =
+        React.useContext(SubscriptionContext);
 
-export const SubscriptionGeneral: FC<SubscriptionGeneralProps> = ({
-    subscriptionBlock,
-}) => {
-    // const { subscriptionData, loadingStatus } =
-    //     React.useContext(SubscriptionContext);
+    console.log(loadingStatus);
+    if (!loadingStatus) {
+        return (
+            <>
+                <EuiSpacer size={'m'} />
+                <EuiLoadingContent />
+            </>
+        );
+    }
 
-    // console.log(loadingStatus);
-    // if (!loadingStatus) {
-    //     return (
-    //         <>
-    //             <EuiSpacer size={'m'} />
-    //             <EuiLoadingContent />
-    //         </>
-    //     );
-    // }
-    console.log(subscriptionBlock);
     return (
         <EuiFlexGrid direction={'row'}>
             <>
-                {subscriptionBlock && (
-                    <EuiFlexItem>
-                        <SubscriptionBlock
-                            title={'Subscription details'}
-                            subscriptionBlock={subscriptionBlock}
-                        />
-                    </EuiFlexItem>
-                )}
-                {/*<EuiFlexItem>*/}
-                {/*    {FixedInputBlock(*/}
-                {/*        'Fixed inputs',*/}
-                {/*        fixedInputs,*/}
-                {/*    )}*/}
-                {/*</EuiFlexItem>*/}
+                <EuiFlexItem>
+                    {SubscriptionBlock(
+                        'Subscription details',
+                        subscriptionData.subscription,
+                    )}
+                </EuiFlexItem>
+                <EuiFlexItem>
+                    {FixedInputBlock(
+                        'Fixed inputs',
+                        subscriptionData.subscription.fixedInputs,
+                    )}
+                </EuiFlexItem>
             </>
         </EuiFlexGrid>
     );
