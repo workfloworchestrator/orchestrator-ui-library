75e957935fc8de5b97301ace3adb84e62dd1f029
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-03 15:37:51 +0200

    Add links found in design to leftNav components

--- libs/orchestrator-ui-components/src/lib/components/OrchestratorPageTemplate/OrchestratorSidebar/OrchestratorSidebar.tsx
+++ libs/orchestrator-ui-components/src/lib/components/OrchestratorPageTemplate/OrchestratorSidebar/OrchestratorSidebar.tsx
@@ -1,7 +1,5 @@
 import React, { FC } from 'react';
 import { EuiButton, EuiSideNav, EuiSpacer } from '@elastic/eui';
-import { useRouter } from 'next/router'
-
 
 export interface OrchestratorSidebarProps {
     routeTo: (route: string) => void;
@@ -9,138 +7,140 @@ export interface OrchestratorSidebarProps {
 
 export const OrchestratorSidebar: FC<OrchestratorSidebarProps> = ({
     routeTo,
-}) => (
-    
-    <EuiSideNav
-        mobileTitle="Nav Items"
-        isOpenOnMobile={false}
-        items={[
-            {
-                renderItem: () => (
-                    <>
-                        <EuiButton
-                            onClick={(
-                                e: React.MouseEvent<
-                                    HTMLButtonElement | HTMLElement,
-                                    MouseEvent
-                                >,
-                            ) => {
+}) => {
+    return (
+        <EuiSideNav
+            mobileTitle="Nav Items"
+            isOpenOnMobile={false}
+            items={[
+                {
+                    renderItem: () => (
+                        <>
+                            <EuiButton
+                                onClick={(
+                                    e: React.MouseEvent<
+                                        HTMLButtonElement | HTMLElement,
+                                        MouseEvent
+                                    >,
+                                ) => {
+                                    e.preventDefault();
+                                    routeTo('/new-process');
+                                }}
+                                iconType="plus"
+                                fullWidth
+                            >
+                                New Process
+                            </EuiButton>
+                            <EuiSpacer size="m" />
+                        </>
+                    ),
+                    name: 'Menu',
+                    id: '1',
+                    items: [
+                        {
+                            name: 'Start',
+                            id: '2',
+                            onClick: (e) => {
                                 e.preventDefault();
-                                routeTo('/new-process');
-                            }}
-                            iconType="plus"
-                            fullWidth
-                        >
-                            New Process
-                        </EuiButton>
-                        <EuiSpacer size="m" />
-                    </>
-                ),
-                name: 'Menu',
-                id: '1',
-                items: [
-                    {
-                      name: 'Start',
-                      id: '2',
-                      onClick: (e) => {
-                          e.preventDefault();
-                          routeTo('/');
-                      },
-                      href: '/',
-                    },
-                    {
-                        name: 'Processes',
-                        id: '3',
-                        onClick: (e) => {
-                            e.preventDefault();
-                            alert('TODO')
-                        },
-                        href: '/',
-                    },
-                    {
-                        name: 'Subscriptions',
-                        id: '4',
-                        onClick: (e) => {
-                            e.preventDefault();
-                            routeTo('/subscriptions');
-                        },
-                        href: '/subscriptions',
-                    },
-                    {
-                        name: 'Metadata',
-                        id: '5',
-                        items: [
-                            {
-                                name: 'Products',
-                                id: '5.1',
-                                onClick: (e) => {
-                                  e.preventDefault();
-                                  routeTo('/metadata/products');
-                                },
+                                routeTo('/');
                             },
-                            {
-                              name: 'Productblocks',
-                              id: '5.2',
-                              onClick: (e) => {
+                            href: '/',
+                        },
+                        {
+                            name: 'Processes',
+                            id: '3',
+                            onClick: (e) => {
                                 e.preventDefault();
-                                routeTo('/metadata/productblocks');
-                              },
+                                alert('TODO');
                             },
-                            {
-                                name: 'Resource types',
-                                id: '5.3',
-                                onClick: (e) => {
-                                  e.preventDefault();
-                                  routeTo('/metadata/resource-types');
-                                },
-                            },
-                            {
-                                name: 'Fixed inputs',
-                                id: '5.4',
-                                onClick: (e) => {
-                                  e.preventDefault();
-                                  routeTo('/metadata/fixed-inputs');
-                                },
-                            },
-                            {
-                              name: 'Workflows',
-                              id: '5.5',
-                              onClick: (e) => {
+                            href: '/',
+                        },
+                        {
+                            name: 'Subscriptions',
+                            id: '4',
+                            onClick: (e) => {
                                 e.preventDefault();
-                                routeTo('/metadata/workflows');
-                              },
-                          },
-                        ]
-                    },
-                    {
-                        name: 'Tasks',
-                        id: '6',
-                        onClick: (e) => {
-                            e.preventDefault();
-                            alert('TODO')
+                                routeTo('/subscriptions');
+                            },
+                            href: '/subscriptions',
                         },
-                        href: '/',                      
-                    },
-                    {
-                        name: 'Settings',
-                        id: '7',
-                        onClick: (e) => {
-                            e.preventDefault();
-                            routeTo('/settings');
+                        {
+                            name: 'Metadata',
+                            id: '5',
+                            items: [
+                                {
+                                    name: 'Products',
+                                    id: '5.1',
+                                    onClick: (e) => {
+                                        e.preventDefault();
+                                        routeTo('/metadata/products');
+                                    },
+                                    isSelected: true,
+                                },
+                                {
+                                    name: 'Productblocks',
+                                    id: '5.2',
+                                    onClick: (e) => {
+                                        e.preventDefault();
+                                        routeTo('/metadata/productblocks');
+                                    },
+                                },
+                                {
+                                    name: 'Resource types',
+                                    id: '5.3',
+                                    onClick: (e) => {
+                                        e.preventDefault();
+                                        routeTo('/metadata/resource-types');
+                                    },
+                                },
+                                {
+                                    name: 'Fixed inputs',
+                                    id: '5.4',
+                                    onClick: (e) => {
+                                        e.preventDefault();
+                                        routeTo('/metadata/fixed-inputs');
+                                    },
+                                },
+                                {
+                                    name: 'Workflows',
+                                    id: '5.5',
+                                    onClick: (e) => {
+                                        e.preventDefault();
+                                        routeTo('/metadata/workflows');
+                                    },
+                                },
+                            ],
                         },
-                        href: '/settings',
-                    },
-                    {
-                        name: 'LIR prefixes',
-                        id: '8',
-                        onClick: (e) => {
-                            e.preventDefault();
-                            alert('TODO')
+                        {
+                            name: 'Tasks',
+                            id: '6',
+                            onClick: (e) => {
+                                e.preventDefault();
+                                alert('TODO');
+                            },
+                            href: '/',
                         },
-                        href: '/',
-                    }
-                ],
-            },
-        ]}
-    />
-);
+                        {
+                            name: 'Settings',
+                            id: '7',
+                            onClick: (e) => {
+                                e.preventDefault();
+                                routeTo('/settings');
+                            },
+                            href: '/settings',
+                        },
+                        {
+                            name: 'LIR prefixes',
+                            id: '8',
+                            onClick: (e) => {
+                                e.preventDefault();
+                                alert('TODO');
+                            },
+                            href: '/',
+                        },
+                    ],
+                },
+            ]}
+        />
+    );
+};
