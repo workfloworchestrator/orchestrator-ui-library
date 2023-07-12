f754a882bdbaa511eec717988fe361a289395ab6
tjeerddie <tjeerdverschragen@hotmail.com>
2023-07-12 13:50:39 +0200

    Upgrade nx to version 16 (#140)
    * upgrade nx to version 16
    
    * Remove upgrade generated files that are unnecessary
    
    * Upgrade lib package version
    
    ---------
    
    Co-authored-by: Tjeerd.Verschragen <tjeerd.verschragen@surf.nl>

--- libs/orchestrator-ui-components/.storybook/main.js
+++ libs/orchestrator-ui-components/.storybook/main.js
@@ -4,11 +4,13 @@ module.exports = {
     ...rootMain,
     core: { ...rootMain.core, builder: 'webpack5' },
     stories: [
-        ...rootMain.stories,
         '../src/lib/**/*.stories.mdx',
         '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
     ],
-    addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
+    addons: [
+        '@storybook/addon-essentials',
+        'storybook-addon-mock',
+    ],
     webpackFinal: async (config, { configType }) => {
         // apply any global webpack configs that might have been specified in .storybook/main.js
         if (rootMain.webpackFinal) {
