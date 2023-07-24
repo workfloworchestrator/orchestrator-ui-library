fcc7566947bf677d27c7442d45f25c015485439e
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-24 13:49:38 +0200

    147-turbo-setup: Remove all unused folders

--- nx-old/link-start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-npx nx build orchestrator-ui-components && npx yalc publish --push dist/libs/orchestrator-ui-components
-npx nx watch --projects=orchestrator-ui-components -- 'npx nx build orchestrator-ui-components && npx yalc publish --push dist/libs/orchestrator-ui-components'
-## After stopping yalc you can optionally remove the yalc store at this location "npx yalc dir"
