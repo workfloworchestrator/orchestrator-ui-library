4c991dc6d851493de47560000912ff35ba03c1d8
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-07-26 12:47:30 +0200

    147 Moves Jest config to app and lib resulting in duplicate config

--- packages/orchestrator-ui-components/jest.config.js
+++ packages/orchestrator-ui-components/jest.config.js
@@ -1,7 +1,16 @@
-const base = require('../../jest.config');
-
 module.exports = {
-    ...base,
+    resetMocks: true,
+    coveragePathIgnorePatterns: [],
+    collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts,tsx}'],
+    coverageThreshold: null,
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'esbuild-jest',
+        '^.+\\.jsx?$': 'esbuild-jest',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
     name: 'orchestrator-ui-components',
     displayName: 'Orchestrator UI Components Tests',
 };
