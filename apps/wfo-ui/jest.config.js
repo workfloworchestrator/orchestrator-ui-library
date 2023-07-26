5aa01e69d16d5eb217bb72ff678f3cd25d28ca5e
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-07-26 11:30:32 +0200

    147 Adds Jest config in the root of the monorepo

---
+++ apps/wfo-ui/jest.config.js
@@ -0,0 +1,7 @@
+const base = require('../../jest.config');
+
+module.exports = {
+    ...base,
+    name: 'wfo-ui',
+    displayName: 'WFO-UI Tests',
+};
---
+++ packages/orchestrator-ui-components/jest.config.js
@@ -0,0 +1,7 @@
+const base = require('../../jest.config');
+
+module.exports = {
+    ...base,
+    name: 'orchestrator-ui-components',
+    displayName: 'Orchestrator UI Components Tests',
+};
