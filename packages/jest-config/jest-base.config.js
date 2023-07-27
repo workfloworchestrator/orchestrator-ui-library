f3082ae94b90e64cd5d789f90fffc1664d61fd70
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-07-27 12:36:20 +0200

    147 setup jest, github actions and husky (#170)
    * 147 Adds Jest config in the root of the monorepo
    
    * 147 Moves Jest config to app and lib resulting in duplicate config
    
    * 147 Updates and runs Prettier config
    
    * 147 Moves jest config into jest-config package
    
    * 147 Adds github actions
    
    * 147 Renames jest config package
    
    * 147 Updates cache key wit package.json hash
    
    * 147 Adds and configures precommit hook

---
+++ packages/jest-config/jest-base.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    resetMocks: true,
+    coveragePathIgnorePatterns: [],
+    collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts,tsx}'],
+    coverageThreshold: null,
+    testEnvironment: 'jsdom',
+    transform: {
+        '^.+\\.tsx?$': 'esbuild-jest',
+        '^.+\\.jsx?$': 'esbuild-jest',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-dom'],
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+};
---
+++ packages/orchestrator-ui-components/jest.config.cjs
@@ -0,0 +1,7 @@
+const base = require('@orchestrator-ui/jest-config/jest-base.config.js');
+
+module.exports = {
+    ...base,
+    name: 'orchestrator-ui-components',
+    displayName: 'Orchestrator UI Components Tests',
+};
