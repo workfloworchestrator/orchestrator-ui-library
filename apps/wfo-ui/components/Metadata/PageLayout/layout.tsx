1120ff51106dd22d4aa39f4ea3b774868aa54253
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-27 16:56:45 +0200

    119-next-intl: Translate metadata tabs

--- apps/wfo-ui/components/Metadata/PageLayout/layout.tsx
+++ apps/wfo-ui/components/Metadata/PageLayout/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import type { ReactNode } from 'react';
 import { EuiSpacer, EuiPageHeader, EuiTab, EuiTabs } from '@elastic/eui';
 import { useRouter } from 'next/router';
+import { useTranslations } from 'next-intl';
 
 interface MetadataLayoutProps {
     children: ReactNode;
@@ -9,32 +10,33 @@ interface MetadataLayoutProps {
 
 export const MetaDataLayout = ({ children }: MetadataLayoutProps) => {
     const router = useRouter();
+    const t = useTranslations('metadata.tabs')
     const currentPath = router.pathname;
 
     const tabs = [
         {
             id: 1,
-            name: 'Products',
+            name: t('products'),
             path: '/metadata/products',
         },
         {
             id: 2,
-            name: 'Productblocks',
+            name: t('productBlocks'),
             path: '/metadata/productblocks',
         },
         {
             id: 3,
-            name: 'Resource types',
+            name: t('resourceTypes'),
             path: '/metadata/resource-types',
         },
         {
             id: 4,
-            name: 'Fixed inputs',
+            name: t('fixedInputs'),
             path: '/metadata/fixed-inputs',
         },
         {
             id: 5,
-            name: 'Workflows',
+            name: t('workflows'),
             path: '/metadata/workflows',
         },
     ];
