a2dcb1bf51bede77dd3006b571491de97d122896
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-28 15:00:03 +0200

    119-next-intl: Style fixes

--- apps/wfo-ui/next.config.js
+++ apps/wfo-ui/next.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     reactStrictMode: false,
     i18n: {
-      // These are all the locales you want to support in
-      // your application
-      locales: ['en-US', 'nl-NL'],
-      defaultLocale: 'en-US',
-    }
+        // These are all the locales you want to support in
+        // your application
+        locales: ['en-US', 'nl-NL'],
+        defaultLocale: 'en-US',
+    },
 };
