1983b48031b07d64b810c8b050c86a067fb16961
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-31 13:22:31 +0200

    86-md-productblocks-page: Basic setup of page in ui package

--- apps/wfo-ui/pages/metadata/productblocks.tsx
+++ apps/wfo-ui/pages/metadata/productblocks.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { ProductBlocksPage as Page } from '@orchestrator-ui/orchestrator-ui-components';
 
-export const ProductBlocksPage = () => (
-    <Page />
-);
+export const ProductBlocksPage = () => <Page />;
 
 export default ProductBlocksPage;
