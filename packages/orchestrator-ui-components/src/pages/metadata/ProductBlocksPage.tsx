9aad13afc4188b8c547adb20d00c9c0d9a83a17b
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-07-31 13:22:31 +0200

    86-md-productblocks-page: Basic setup of page in ui package

--- packages/orchestrator-ui-components/src/pages/metadata/ProductBlocksPage.tsx
+++ packages/orchestrator-ui-components/src/pages/metadata/ProductBlocksPage.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from 'react';
 
 export const ProductBlocksPage = () => {
-  return (
-    <div>ProductBLOCKSpaGE!!!!</div>
-  )
-}
+    return <div>ProductBLOCKSpaGE!!!!</div>;
+};
