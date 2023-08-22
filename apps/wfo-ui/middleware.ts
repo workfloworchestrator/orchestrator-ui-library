41fcce3a879847fc1a1160341d2f6d583596f584
René Dohmen <acidjunk@gmail.com>
2023-08-22 16:07:36 +0200

    Redirect instead of rewrite

--- apps/wfo-ui/middleware.ts
+++ apps/wfo-ui/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-    return NextResponse.rewrite(new URL('/metadata/products', request.url));
+    return NextResponse.redirect(new URL('/metadata/products', request.url));
 }
 
 // Supports both a single string value or an array of matchers
