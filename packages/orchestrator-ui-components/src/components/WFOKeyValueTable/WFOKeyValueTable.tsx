6a5fad4a78e2ef12bbd07ed4a158ec57709b75d3
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-08-24 15:43:23 +0200

    211 - Breaks table up in key component and a value component

--- packages/orchestrator-ui-components/src/components/WFOKeyValueTable/WFOKeyValueTable.tsx
+++ packages/orchestrator-ui-components/src/components/WFOKeyValueTable/WFOKeyValueTable.tsx
@@ -20,57 +20,81 @@ export const WFOKeyValueTable: FC<WFOKeyValueTableProps> = ({
     showCopyToClipboardIcon = false,
 }) => {
     const { theme } = useOrchestratorTheme();
+    const { keyValueTable } = getStyles(theme);
+
+    return (
+        <div css={keyValueTable}>
+            {keyValues.map(({ key, value, plainTextValue }, rowNumber) => (
+                <>
+                    <WFOKeyCell value={key} rowNumber={rowNumber} />
+                    <WFOValueCell
+                        value={value}
+                        plainTextValue={plainTextValue}
+                        rowNumber={rowNumber}
+                        enableCopyIcon={showCopyToClipboardIcon}
+                    />
+                </>
+            ))}
+        </div>
+    );
+};
+
+type WFOKeyCellProps = {
+    value: string;
+    rowNumber: number;
+};
+const WFOKeyCell: FC<WFOKeyCellProps> = ({ value, rowNumber }) => {
+    const { theme } = useOrchestratorTheme();
+    const { keyColumnStyle, getBackgroundColorStyleForRow } = getStyles(theme);
+
+    return (
+        <div css={[getBackgroundColorStyleForRow(rowNumber), keyColumnStyle]}>
+            <b>{value}</b>
+        </div>
+    );
+};
+
+type WFOValueCellProps = {
+    value: ReactNode;
+    plainTextValue?: string;
+    rowNumber: number;
+    enableCopyIcon: boolean;
+};
+
+const WFOValueCell: FC<WFOValueCellProps> = ({
+    value,
+    plainTextValue,
+    rowNumber,
+    enableCopyIcon,
+}) => {
+    const { theme } = useOrchestratorTheme();
     const {
-        keyColumnStyle,
         valueColumnStyle,
         clipboardIconStyle,
         clickable,
-        keyValueTable,
         getBackgroundColorStyleForRow,
     } = getStyles(theme);
+
+    const shouldRenderCopyColumn = enableCopyIcon && plainTextValue;
+
     return (
-        <div css={keyValueTable}>
-            {keyValues.map(({ key, value, plainTextValue }, rowNumber) => {
-                const shouldRenderCopyColumn =
-                    showCopyToClipboardIcon && plainTextValue;
-                return (
-                    <>
-                        <div
-                            css={[
-                                getBackgroundColorStyleForRow(rowNumber),
-                                keyColumnStyle,
-                            ]}
-                        >
-                            <b>{key}</b>
-                        </div>
-                        <div
-                            css={[
-                                getBackgroundColorStyleForRow(rowNumber),
-                                valueColumnStyle,
-                            ]}
-                        >
-                            <div>{value}</div>
-                            <div css={clipboardIconStyle}>
-                                {shouldRenderCopyColumn && (
-                                    <EuiCopy textToCopy={plainTextValue}>
-                                        {(copy) => (
-                                            <div onClick={copy} css={clickable}>
-                                                <WFOClipboardCopy
-                                                    width={16}
-                                                    height={16}
-                                                    color={
-                                                        theme.colors.mediumShade
-                                                    }
-                                                />
-                                            </div>
-                                        )}
-                                    </EuiCopy>
-                                )}
+        <div css={[getBackgroundColorStyleForRow(rowNumber), valueColumnStyle]}>
+            <div>{value}</div>
+            <div css={clipboardIconStyle}>
+                {shouldRenderCopyColumn && (
+                    <EuiCopy textToCopy={plainTextValue}>
+                        {(copy) => (
+                            <div onClick={copy} css={clickable}>
+                                <WFOClipboardCopy
+                                    width={16}
+                                    height={16}
+                                    color={theme.colors.mediumShade}
+                                />
                             </div>
-                        </div>
-                    </>
-                );
-            })}
+                        )}
+                    </EuiCopy>
+                )}
+            </div>
         </div>
     );
 };
