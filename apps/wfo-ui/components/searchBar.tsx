a55858d523053ef8d89b0664a9d6e1e2e3ed800b
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-08-29 14:56:52 +0200

    134: Fix linting error

--- apps/wfo-ui/components/searchBar.tsx
+++ apps/wfo-ui/components/searchBar.tsx
@@ -111,6 +111,7 @@ export const SearchBar = ({
                     placeholder: searchPhrase,
                     schema,
                 }}
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
                 // @ts-ignore
                 filters={filters}
                 onChange={onChange}
