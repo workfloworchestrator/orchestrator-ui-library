c32fc7a450db0ffc50d931c90bc5fd0503ee4964
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-09-01 15:55:53 +0200

    151 Moves WFOSubscriptionDetail page to package

--- apps/wfo-ui/pages/subscriptions/[subscriptionId].tsx
+++ apps/wfo-ui/pages/subscriptions/[subscriptionId].tsx
@@ -1,31 +1,12 @@
-import { useRouter } from 'next/router';
 import React from 'react';
 
 import NoSSR from 'react-no-ssr';
-import {
-    WFOSubscription,
-    TreeProvider,
-    SubscriptionProvider,
-} from '@orchestrator-ui/orchestrator-ui-components';
+import { WFOSubscriptionDetailPage } from '@orchestrator-ui/orchestrator-ui-components';
 
 const SubscriptionDetailPage = () => {
-    const router = useRouter();
-    const { subscriptionId } = router.query;
     return (
         <NoSSR>
-            <SubscriptionProvider>
-                <TreeProvider>
-                    {subscriptionId && (
-                        <WFOSubscription
-                            subscriptionId={
-                                Array.isArray(subscriptionId)
-                                    ? subscriptionId[0]
-                                    : subscriptionId
-                            }
-                        />
-                    )}
-                </TreeProvider>
-            </SubscriptionProvider>
+            <WFOSubscriptionDetailPage />
         </NoSSR>
     );
 };
