57f091e355b9ab5cfaa16740bb3c2f660ddbf0e2
René Dohmen <acidjunk@gmail.com>
2023-09-02 06:02:05 +0200

    Refactored forms

--- packages/pydantic-forms/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-const base = require('@orchestrator-ui/jest-config/jest-base.config.js');
-
-module.exports = {
-    ...base,
-    name: 'pydantic-forms',
-    displayName: 'Pydantic Forms Tests',
-};
