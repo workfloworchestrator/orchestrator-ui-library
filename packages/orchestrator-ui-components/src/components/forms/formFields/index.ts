ea053d56182ad1ec5bf032911a73e78e19b01704
René Dohmen <acidjunk@gmail.com>
2023-09-02 06:02:05 +0200

    Refactored forms

---
+++ packages/orchestrator-ui-components/src/components/forms/formFields/index.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019-2023 SURF.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+//@ts-nocheck
+// export { default as AutoFields } from './AutoFields';
+export { default as BoolField } from './BoolField';
+// export { default as DateField } from './DateField';
+// export { default as TimestampField } from './TimestampField';
+// export { default as ErrorField } from './ErrorField';
+// export { default as ErrorsField } from './ErrorsField';
+// export { default as HiddenField } from './HiddenField';
+// export { default as ListAddField } from './ListAddField';
+// export { default as ListDelField } from './ListDelField';
+// export { default as ListField } from './ListField';
+// export { default as ListItemField } from './ListItemField';
+export { default as LongTextField } from './LongTextField';
+// export { default as NestField } from './NestField';
+export { default as NumField } from './NumField';
+// export { default as RadioField } from './RadioField';
+// export { default as SelectField } from './SelectField';
+// export { default as ListSelectField } from './ListSelectField';
+export { default as SubmitField } from './SubmitField';
+export { default as TextField } from './TextField';
+
+// export { default as AcceptField } from './AcceptField';
+export { default as LabelField } from './LabelField';
+export { default as DividerField } from './DividerField';
+// export { default as OptGroupField } from './OptGroupField';
+// export { default as SubscriptionField } from './SubscriptionField';
+// export { default as SubscriptionSummaryField } from './SubscriptionSummaryField';
+// export { default as SummaryField } from './SummaryField';
+
+// export { default as LocationCodeField } from './logic/LocationCodeField';
+// export { default as OrganisationField } from './logic/OrganisationField';
+// export { default as ProductField } from './logic/ProductField';
---
+++ packages/orchestrator-ui-components/src/types/forms.ts
@@ -0,0 +1,40 @@
+import { JSONSchema6 } from 'json-schema';
+import { Ref } from 'react';
+import { HTMLFieldProps } from 'uniforms';
+
+
+export interface ValidationError {
+    input_type: string;
+    loc: (string | number)[];
+    msg: string;
+    type: string;
+    ctx?: ValidationErrorContext;
+}
+
+export interface ValidationErrorContext {
+    [index: string]: string;
+}
+export interface Form {
+    stepUserInput?: JSONSchema6;
+    hasNext?: boolean;
+}
+
+export type InputForm = JSONSchema6;
+export interface FormNotCompleteResponse {
+    form: InputForm;
+    hasNext?: boolean;
+}
+
+export type FieldProps<
+    Value,
+    Extra = {},
+    InputElementType = HTMLInputElement,
+    ElementType = HTMLDivElement,
+> = HTMLFieldProps<
+    Value,
+    ElementType,
+    {
+        inputRef?: Ref<InputElementType>;
+        description?: string;
+    } & Extra
+>;
