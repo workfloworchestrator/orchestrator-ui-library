14e8cb8b70917dc3e60f44d4105c96b903f86ccb
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-09-04 14:07:42 +0200

    224-process-detail: Basic datafetching

--- packages/orchestrator-ui-components/src/graphqlQueries/index.ts
+++ packages/orchestrator-ui-components/src/graphqlQueries/index.ts
@@ -2,3 +2,4 @@ export * from './subscriptionsListQuery';
 export * from './productBlocksQuery';
 export * from './productsQuery';
 export * from './resourceTypesQuery';
+export * from './processDetailQuery';
