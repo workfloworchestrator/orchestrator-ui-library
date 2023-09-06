5b99314a0de822ccf2c5f48bd33e31eebf94b47b
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-09-06 09:57:22 +0200

    224-process-detail: merge fix

--- packages/orchestrator-ui-components/src/graphqlQueries/index.ts
+++ packages/orchestrator-ui-components/src/graphqlQueries/index.ts
@@ -2,8 +2,5 @@ export * from './subscriptionsListQuery';
 export * from './productBlocksQuery';
 export * from './productsQuery';
 export * from './resourceTypesQuery';
-<<<<<<< HEAD
 export * from './subscriptionDetailQuery';
-=======
 export * from './processDetailQuery';
->>>>>>> 14e8cb8 (224-process-detail: Basic datafetching)
