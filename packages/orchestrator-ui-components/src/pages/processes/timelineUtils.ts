26d88de98e845f68352149167f510cf98a8ef9b2
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-09-08 21:42:37 +0200

    223 Renames variables in the timelineUtils file

--- packages/orchestrator-ui-components/src/pages/processes/timelineUtils.ts
+++ packages/orchestrator-ui-components/src/pages/processes/timelineUtils.ts
@@ -13,31 +13,57 @@ const mapStepToTimelineItem = (step: ProcessDetailStep): TimelineItem => {
     return {
         processStepStatus: step.status,
         onClick: () => {
+            // Todo: Implement onClick after step-cards are implemented
+            // https://github.com/workfloworchestrator/orchestrator-ui/issues/225
             console.log(`Clicked on ${step.name} (${step.stepid})`);
         },
     };
 };
 
+const stepsShouldBeMerged = (
+    previousStep: ProcessDetailStep,
+    currentStep: ProcessDetailStep,
+) =>
+    previousStep.status !== StepStatus.COMPLETE &&
+    previousStep.name === currentStep.name;
+
 export const mapProcessStepsToTimelineItems = (steps: ProcessDetailStep[]) =>
-    steps.reduce<TimelineItem[]>((acc: TimelineItem[], curr, index, arr) => {
-        const previousStep = acc.slice(-1)[0];
+    steps.reduce<TimelineItem[]>(
+        (
+            allTimelineItems: TimelineItem[],
+            currentProcessDetailStep,
+            index,
+            allProcessDetailSteps,
+        ) => {
+            const previousTimelineItem = allTimelineItems.slice(-1)[0];
 
-        if (index > 0 && arr[index - 1].name === curr.name) {
-            const allStepsExceptPrevious = acc.slice(0, -1);
-            const updatedPreviousStep: TimelineItem = {
-                ...previousStep,
-                processStepStatus: getMostAccurateTimelineStatus(
-                    previousStep.processStepStatus,
-                    curr.status,
-                ),
-                value:
-                    typeof previousStep.value === 'number'
-                        ? previousStep.value + 1
-                        : 2,
-            };
+            if (
+                index > 0 &&
+                stepsShouldBeMerged(
+                    allProcessDetailSteps[index - 1],
+                    currentProcessDetailStep,
+                )
+            ) {
+                const allStepsExceptPrevious = allTimelineItems.slice(0, -1);
+                const updatedPreviousStep: TimelineItem = {
+                    ...previousTimelineItem,
+                    processStepStatus: getMostAccurateTimelineStatus(
+                        previousTimelineItem.processStepStatus,
+                        currentProcessDetailStep.status,
+                    ),
+                    value:
+                        typeof previousTimelineItem.value === 'number'
+                            ? previousTimelineItem.value + 1
+                            : 2,
+                };
 
-            return [...allStepsExceptPrevious, updatedPreviousStep];
-        }
+                return [...allStepsExceptPrevious, updatedPreviousStep];
+            }
 
-        return [...acc, mapStepToTimelineItem(curr)];
-    }, []);
+            return [
+                ...allTimelineItems,
+                mapStepToTimelineItem(currentProcessDetailStep),
+            ];
+        },
+        [],
+    );
