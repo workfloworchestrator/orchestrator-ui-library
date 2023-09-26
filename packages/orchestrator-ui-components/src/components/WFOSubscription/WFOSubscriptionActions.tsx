cb13309dbe0b33b9c2798a263dc96d3a237afd0e
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-09-26 16:33:48 +0200

    291: Add navigation to start-workflow page to actions dropdown in subscription detail

--- packages/orchestrator-ui-components/src/components/WFOSubscription/WFOSubscriptionActions.tsx
+++ packages/orchestrator-ui-components/src/components/WFOSubscription/WFOSubscriptionActions.tsx
@@ -1,4 +1,6 @@
 import React, { FC, useState } from 'react';
+import Link from 'next/link';
+
 import {
     EuiButton,
     EuiContextMenuItem,
@@ -9,20 +11,17 @@ import {
     EuiPopover,
 } from '@elastic/eui';
 import { useTranslations } from 'next-intl';
-import { useSubscriptionActions } from '../../hooks/useSubscriptionActions';
+import {
+    SubscriptionAction,
+    useSubscriptionActions,
+} from '../../hooks/useSubscriptionActions';
 
 type MenuItemProps = {
     key: string;
     icon: string;
-    description: string;
+    action: SubscriptionAction;
     index: number;
 };
-
-const MenuItem: FC<MenuItemProps> = ({ icon, description, key }) => (
-    <EuiContextMenuItem key={key} icon={<EuiAvatar name={icon} size="s" />}>
-        {description}
-    </EuiContextMenuItem>
-);
 
 type MenuBlockProps = {
     title: string;
@@ -40,6 +39,24 @@ export type SubscriptionActionsProps = {
 export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
     subscriptionId,
 }) => {
+    const MenuItem: FC<MenuItemProps> = ({ icon, action, key }) => {
+        return (
+            <Link
+                href={{
+                    pathname: `/start-workflow/${action.name}`,
+                    query: { subscriptionId: subscriptionId },
+                }}
+            >
+                <EuiContextMenuItem
+                    key={key}
+                    icon={<EuiAvatar name={icon} size="s" />}
+                >
+                    {action.description}
+                </EuiContextMenuItem>
+            </Link>
+        );
+    };
+
     const t = useTranslations('subscriptions.detail.workflow');
     const [isPopoverOpen, setPopover] = useState(false);
     const { data: subscriptionActions } =
@@ -59,7 +76,7 @@ export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
             iconSide="right"
             onClick={onButtonClick}
         >
-            Actions
+            {t('actions')}
         </EuiButton>
     );
 
@@ -77,11 +94,11 @@ export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
                     {subscriptionActions && subscriptionActions.create && (
                         <>
                             <MenuBlock title={t('create')}></MenuBlock>
-                            {subscriptionActions.create.map((item, index) => (
+                            {subscriptionActions.create.map((action, index) => (
                                 <MenuItem
                                     key={`c_${index}`}
                                     icon={'CreateLong'}
-                                    description={item.description}
+                                    action={action}
                                     index={index}
                                 />
                             ))}
@@ -91,11 +108,11 @@ export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
                     {subscriptionActions && subscriptionActions.modify && (
                         <>
                             <MenuBlock title={t('modify')}></MenuBlock>
-                            {subscriptionActions.modify.map((item, index) => (
+                            {subscriptionActions.modify.map((action, index) => (
                                 <MenuItem
                                     key={`m_${index}`}
                                     icon={'M'}
-                                    description={item.description}
+                                    action={action}
                                     index={index}
                                 />
                             ))}
@@ -105,11 +122,11 @@ export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
                     {subscriptionActions && subscriptionActions.system && (
                         <>
                             <MenuBlock title={t('system')}></MenuBlock>
-                            {subscriptionActions.system.map((item, index) => (
+                            {subscriptionActions.system.map((action, index) => (
                                 <MenuItem
                                     key={`s_${index}`}
                                     icon={'System'}
-                                    description={item.description}
+                                    action={action}
                                     index={index}
                                 />
                             ))}
@@ -120,11 +137,11 @@ export const WFOSubscriptionActions: FC<SubscriptionActionsProps> = ({
                         <>
                             <MenuBlock title={t('terminate')}></MenuBlock>
                             {subscriptionActions.terminate.map(
-                                (item, index) => (
+                                (action, index) => (
                                     <MenuItem
                                         key={`t_${index}`}
                                         icon={'Terminate'}
-                                        description={item.description}
+                                        action={action}
                                         index={index}
                                     />
                                 ),
