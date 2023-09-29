308b39c490407020f0017d57c63ae3d724c75cfd
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-09-29 19:44:18 +0200

    225 Updates timeline-item mapper and updates unit tests

--- packages/orchestrator-ui-components/src/pages/processes/WFOProcessDetailPage.tsx
+++ packages/orchestrator-ui-components/src/pages/processes/WFOProcessDetailPage.tsx
@@ -8,8 +8,15 @@ import { TimelineItem, WFOLoading } from '../../components';
 import { WFOProcessDetail } from './WFOProcessDetail';
 import { WFOStepList, WFOStepListRef } from '../../components/WFOSteps';
 
-import { mapProcessStepsToTimelineItems } from './timelineUtils';
-import { Step, StepStatus } from '../../types';
+import {
+    convertStepsToGroupedSteps,
+    mapGroupedStepsToTimelineItems,
+} from './timelineUtils';
+import { Step } from '../../types';
+
+export type GroupedStep = {
+    steps: Step[];
+};
 
 interface WFOProcessDetailPageProps {
     processId: string;
@@ -31,64 +38,12 @@ export const WFOProcessDetailPage = ({
     const process = data?.processes.page[0];
     const steps = process?.steps ?? [];
 
-    // Todo avoiding the mapper
-    const timelineItems: TimelineItem[] = mapProcessStepsToTimelineItems(steps);
-
     const productNames = getProductNamesFromProcess(process);
     const pageTitle = isFetching ? '...' : process?.workflowName || '';
 
-    type GroupedStep = {
-        steps: Step[];
-        // ref: React.RefObject<HTMLDivElement>;
-        // future: add details concerning the group e.g. for the tooltip content in timeline
-    };
-
-    // Todo move to utils -- currently in packages/orchestrator-ui-components/src/pages/processes/timelineUtils.ts
-    // isUnRetryableState
-    const isFinalStepStatus = (status: StepStatus): boolean => {
-        return (
-            status === StepStatus.COMPLETE ||
-            status === StepStatus.SUCCESS ||
-            status === StepStatus.SKIPPED
-        );
-    };
-    // Todo move to utils -- currently in packages/orchestrator-ui-components/src/pages/processes/timelineUtils.ts
-    const stepsShouldBeMerged = (previousStep: Step, currentStep: Step) =>
-        !isFinalStepStatus(previousStep.status) &&
-        previousStep.name === currentStep.name;
-
-    const groupedSteps: GroupedStep[] = steps.reduce<GroupedStep[]>(
-        (previousGroupedSteps: GroupedStep[], currentStep, index, allSteps) => {
-            const previousGroupedStep = previousGroupedSteps.slice(-1)[0];
-
-            if (
-                index > 0 &&
-                stepsShouldBeMerged(allSteps[index - 1], currentStep)
-            ) {
-                const allGroupedStepsExceptLast = previousGroupedSteps.slice(
-                    0,
-                    -1,
-                );
-
-                const updatedLastGroupedStep: GroupedStep = {
-                    // ref: previousGroupedStep.ref,
-                    steps: [...previousGroupedStep.steps, currentStep],
-                };
-
-                return [...allGroupedStepsExceptLast, updatedLastGroupedStep];
-            }
-
-            return [
-                ...previousGroupedSteps,
-                {
-                    // Todo: react throws warning: the order of the hooks is not consistent
-                    // ref: useRef<HTMLDivElement>(null),
-                    steps: [currentStep],
-                },
-            ];
-        },
-        [],
-    );
+    const groupedSteps: GroupedStep[] = convertStepsToGroupedSteps(steps);
+    const timelineItems: TimelineItem[] =
+        mapGroupedStepsToTimelineItems(groupedSteps);
 
     return (
         <WFOProcessDetail
