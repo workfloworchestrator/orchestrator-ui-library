727aff66ba1908bbdcabfadc8fa95d23bf19ce15
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-10-10 13:57:20 +0200

    281-related-subscriptions: Add search strike through icon

---
+++ packages/orchestrator-ui-components/src/icons/WFOSearchStrikethrough.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { WFOSearchStrikethrough } from './WFOSearchStrikethrough';
+
+const Story: Meta<typeof WFOSearchStrikethrough> = {
+    component: WFOSearchStrikethrough,
+    title: 'Icons/WFOSearchStrikethrough',
+};
+export default Story;
+
+export const Primary = {
+    args: {},
+};
