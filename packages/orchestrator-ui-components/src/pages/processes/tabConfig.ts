3b4794c46e346d3a0e34c2204f2b3dccb9323c1f
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-10-12 14:38:03 +0200

    345 - Applies always-on filter on process list page

--- packages/orchestrator-ui-components/src/pages/processes/tabConfig.ts
+++ packages/orchestrator-ui-components/src/pages/processes/tabConfig.ts
@@ -15,6 +15,15 @@ export const defaultProcessListTabs: WFOFilterTab<
         translationKey: 'active',
         alwaysOnFilters: [
             {
+                // Todo: isTask is not a key of Process
+                // However, backend still supports it. Field should not be a keyof ProcessListItem (or process)
+                // https://github.com/workfloworchestrator/orchestrator-ui/issues/290
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore waiting for fix in backend
+                field: 'isTask',
+                value: 'false',
+            },
+            {
                 field: 'lastStatus',
                 value: 'created-running-suspended-waiting-failed-resumed',
             },
@@ -24,6 +33,15 @@ export const defaultProcessListTabs: WFOFilterTab<
         id: WFOProcessListTabType.COMPLETED,
         translationKey: 'completed',
         alwaysOnFilters: [
+            {
+                // Todo: isTask is not a key of Process
+                // However, backend still supports it. Field should not be a keyof ProcessListItem (or process)
+                // https://github.com/workfloworchestrator/orchestrator-ui/issues/290
+                // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                // @ts-ignore waiting for fix in backend
+                field: 'isTask',
+                value: 'false',
+            },
             {
                 field: 'lastStatus',
                 value: 'completed',
