3a2933f1d50793b2b249b3d54513fd226d0e94ef
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-10-12 15:44:46 +0200

    281-related-subscriptions: Set default tab back to general

--- packages/orchestrator-ui-components/src/components/WFOSubscription/WFOSubscription.tsx
+++ packages/orchestrator-ui-components/src/components/WFOSubscription/WFOSubscription.tsx
@@ -26,7 +26,7 @@ type WFOSubscriptionProps = {
 export const WFOSubscription = ({ subscriptionId }: WFOSubscriptionProps) => {
     const t = useTranslations('subscriptions.detail');
     const [selectedTabId, setSelectedTabId] = useState<SubscriptionTabIds>(
-        SubscriptionTabIds.RELATED_SUBSCRIPTIONS_TAB,
+        SubscriptionTabIds.GENERAL_TAB,
     );
 
     const { data, isFetching } = useQueryWithGraphql(
