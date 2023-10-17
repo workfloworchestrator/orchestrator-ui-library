963a0bc30f1a000d2ee45a8096968e5560e82dd0
Tjeerd.Verschragen <tjeerd.verschragen@surf.nl>
2023-10-17 14:56:28 +0200

    Change custom provider to allow pkce without needing clientSecret

--- apps/wfo-ui/pages/api/auth/[...nextauth].ts
+++ apps/wfo-ui/pages/api/auth/[...nextauth].ts
@@ -5,27 +5,34 @@ export type SessionToken = Session & {
     accessToken?: unknown;
 };
 
+const token_endpoint_auth_method = process.env.NEXTAUTH_CLIENT_SECRET
+    ? 'client_secret_basic'
+    : 'none';
+
 export const authOptions: AuthOptions = {
     providers: [
         {
             id: process.env.NEXTAUTH_ID || '',
             name: process.env.NEXTAUTH_ID || '',
             clientId: process.env.NEXTAUTH_CLIENT_ID || '',
-            clientSecret: process.env.NEXTAUTH_SECRET || '',
+            clientSecret: process.env.NEXTAUTH_CLIENT_SECRET || undefined,
             wellKnown: `${
                 process.env.NEXTAUTH_ISSUER || ''
             }/.well-known/openid-configuration`,
             type: 'oauth',
-            authorization: { params: { scope: 'openid' } },
-            checks: ['pkce'],
+            authorization: { params: { scope: 'openid profile' } },
+            checks: ['pkce', 'state'],
             idToken: true,
             profile(profile) {
                 return {
                     id: profile.sub,
                     name: profile.name ?? profile.preferred_username,
                     email: profile.email,
-                    image: profile.picture,
                 };
+            },
+            client: {
+                token_endpoint_auth_method,
+                response_types: ['code'],
             },
         },
     ],
