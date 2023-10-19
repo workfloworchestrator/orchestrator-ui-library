f2a53d257b5586814ab8ad5a42d890c961f53ee2
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-10-19 11:16:02 +0200

    81 Moves style to styles file for WFOTableHeaderCell

--- packages/orchestrator-ui-components/src/components/WFOTable/WFOBasicTable/WFOTableHeaderCell.tsx
+++ packages/orchestrator-ui-components/src/components/WFOTable/WFOBasicTable/WFOTableHeaderCell.tsx
@@ -9,8 +9,9 @@ import {
     EuiText,
     useGeneratedHtmlId,
 } from '@elastic/eui';
-import { useOrchestratorTheme } from '../../../hooks';
+import { useWithOrchestratorTheme } from '../../../hooks';
 import { WFOSortButtons } from '../WFOSortButtons';
+import { getStyles } from './styles';
 
 export type WFOTableHeaderCellProps = {
     fieldName: string;
@@ -29,7 +30,14 @@ export const WFOTableHeaderCell: FC<WFOTableHeaderCellProps> = ({
     onSetSortOrder,
     onSearch,
 }) => {
-    const { theme } = useOrchestratorTheme();
+    const {
+        headerCellContentStyle,
+        headerCellPopoverHeaderStyle,
+        headerCellPopoverHeaderTitleStyle,
+        headerCellPopoverContentStyle,
+        getHeaderCellButtonStyle,
+    } = useWithOrchestratorTheme(getStyles);
+
     const smallContextMenuPopoverId = useGeneratedHtmlId({
         prefix: 'smallContextMenuPopover',
     });
@@ -52,18 +60,10 @@ export const WFOTableHeaderCell: FC<WFOTableHeaderCellProps> = ({
         }
     };
 
-    const PopoverButton = () => (
+    const HeaderCellContentButton = () => (
         <button onClick={handleButtonClick} disabled={!isSortable}>
-            <div
-                css={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    cursor: isSortable ? 'pointer' : 'not-allowed',
-                }}
-            >
-                <div css={{ fontWeight: theme.font.weight.semiBold }}>
-                    {children}
-                </div>
+            <div css={getHeaderCellButtonStyle(isSortable)}>
+                <div css={headerCellContentStyle}>{children}</div>
                 {sortOrder && (
                     <WFOSortDirectionIcon sortDirection={sortOrder} />
                 )}
@@ -71,44 +71,42 @@ export const WFOTableHeaderCell: FC<WFOTableHeaderCellProps> = ({
         </button>
     );
 
+    const PopoverHeader = () => (
+        <div css={headerCellPopoverHeaderStyle}>
+            <EuiText size="xs" css={headerCellPopoverHeaderTitleStyle}>
+                {children}
+            </EuiText>
+            <WFOSortButtons
+                sortOrder={sortOrder}
+                onChangeSortOrder={handleChangeSortOrder}
+            />
+        </div>
+    );
+
+    const PopoverContent = () => (
+        <div css={headerCellPopoverContentStyle}>
+            <EuiFieldSearch
+                className={fieldName}
+                placeholder="Search"
+                onSearch={handleSearch}
+                isClearable={false}
+            />
+        </div>
+    );
+
     return (
         <EuiPopover
             initialFocus={`.euiPanel .euiFieldSearch.${fieldName}`}
             id={smallContextMenuPopoverId}
-            button={<PopoverButton />}
+            button={<HeaderCellContentButton />}
             isOpen={isPopoverOpen}
             closePopover={closePopover}
             panelPaddingSize="none"
             anchorPosition="downLeft"
         >
-            <div
-                css={{
-                    margin: 12,
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    alignItems: 'center',
-                }}
-            >
-                <EuiText
-                    size="xs"
-                    css={{ fontWeight: theme.font.weight.medium }}
-                >
-                    {children}
-                </EuiText>
-                <WFOSortButtons
-                    sortOrder={sortOrder}
-                    onChangeSortOrder={handleChangeSortOrder}
-                />
-            </div>
+            <PopoverHeader />
             <EuiHorizontalRule margin="none" />
-            <div css={{ margin: 12 }}>
-                <EuiFieldSearch
-                    className={fieldName}
-                    placeholder="Search"
-                    onSearch={handleSearch}
-                    isClearable={false}
-                />
-            </div>
+            <PopoverContent />
         </EuiPopover>
     );
 };
