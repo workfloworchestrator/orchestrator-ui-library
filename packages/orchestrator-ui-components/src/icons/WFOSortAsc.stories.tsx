34aa4f58e83c291865743c95f58d68c21b81d53a
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-10-20 16:52:07 +0200

    81 - Adds storybook entries for WFOSortAsc and WFOSortDesc components

---
+++ packages/orchestrator-ui-components/src/icons/WFOSortAsc.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { WFOSortAsc } from './WFOSortAsc';
+
+const Story: Meta<typeof WFOSortAsc> = {
+    component: WFOSortAsc,
+    title: 'Icons/WFOSortAsc',
+};
+export default Story;
+
+export const Primary = {
+    args: {},
+};
---
+++ packages/orchestrator-ui-components/src/icons/WFOSortDesc.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { WFOSortDesc } from './WFOSortDesc';
+
+const Story: Meta<typeof WFOSortDesc> = {
+    component: WFOSortDesc,
+    title: 'Icons/WFOSortDesc',
+};
+export default Story;
+
+export const Primary = {
+    args: {},
+};
