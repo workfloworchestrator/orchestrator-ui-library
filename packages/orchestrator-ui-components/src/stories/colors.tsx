f6bdedc08f1e08d1f9d80618dafc510dec880f1f
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-10-24 10:37:23 +0200

    284: Improve typing and style of colors story

--- packages/orchestrator-ui-components/src/stories/colors.tsx
+++ packages/orchestrator-ui-components/src/stories/colors.tsx
@@ -1,30 +1,25 @@
-import React from 'react'
-import { ColorPalette, ColorItem} from '@storybook/blocks'
-import { useOrchestratorTheme } from '../hooks'
+import React from 'react';
+import { ColorPalette, ColorItem } from '@storybook/blocks';
+import { useOrchestratorTheme } from '../hooks';
 
 export const Colors = () => {
-  const { theme, toSecondaryColor } = useOrchestratorTheme()
-  const colorItems = []
-  const themeColors = theme.colors
+    const { theme, toSecondaryColor } = useOrchestratorTheme();
+    const colorItems = [];
+    const themeColors = theme.colors;
 
-  if(themeColors) {
-    for(const [name, hex] of Object.entries(themeColors)) {
-      const color = hex as string // Casting here because TS gets confused by the LIGHT/DARK theme pattern and cant decide
-      colorItems.push(
-        <ColorItem 
-        key={name}
-        title={name}
-        subtitle='toSecondary'
-        colors={[color, toSecondaryColor(color)]}
-      />    
-      )
+    if (themeColors) {
+        for (const [name, hex] of Object.entries(themeColors)) {
+            const color = hex as string; // Casting here because TS gets confused by the LIGHT/DARK theme pattern and cant decide
+            colorItems.push(
+                <ColorItem
+                    key={name}
+                    title={name}
+                    subtitle="toSecondary"
+                    colors={[color, toSecondaryColor(color)]}
+                />,
+            );
+        }
     }
-  }
 
-  return (
-    <ColorPalette>
-      {colorItems}
-    </ColorPalette>
-  
-  )
-}
+    return <ColorPalette>{colorItems}</ColorPalette>;
+};
