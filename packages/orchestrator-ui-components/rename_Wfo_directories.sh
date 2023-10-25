d1677d5361be11e06ef9c9b0229846a2ebbab625
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-10-25 11:40:58 +0200

    372: Removing the bash scripts used to do the renaming

--- packages/orchestrator-ui-components/rename_Wfo_directories.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-folder='./src/components'
-Wfo_directories=$(find src/components -name '*Wfo*' -type d -not -path './node_modules/*')
-for directory in $Wfo_directories;do
-    new_name=$(echo "$directory" | sed "s/\(.*\)Wfo/\1Wfo/")
-    git mv $directory tmp
-    git mv tmp $new_name
-done
-
