611c1cadb550b722a7841a41ef77e27030eba81e
Wouter Huisman <wouter.huisman@surf.nl>
2023-10-26 15:15:35 +0200

    address paused engine color and status column width

--- packages/orchestrator-ui-components/src/pages/metadata/WfoProductBlocksPage.tsx
+++ packages/orchestrator-ui-components/src/pages/metadata/WfoProductBlocksPage.tsx
@@ -122,7 +122,7 @@ export const WfoProductBlocksPage = () => {
         },
         dependsOn: {
             field: PRODUCT_BLOCK_FIELD_PRODUCT_BLOCKS,
-            name: t('subtendingProductBlocks'),
+            name: t('dependingProductBlocks'),
             render: (dependsOn) => (
                 <>
                     {dependsOn.map((productBlock, index) => (
