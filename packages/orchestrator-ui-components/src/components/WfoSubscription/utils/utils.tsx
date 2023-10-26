2dabc92c57d3e5de6e7c9fa22bc7dbf6fddc24f5
Wouter Huisman <wouter.huisman@surf.nl>
2023-10-26 18:09:54 +0200

    Create WF colour as success color

--- packages/orchestrator-ui-components/src/components/WfoSubscription/utils/utils.tsx
+++ packages/orchestrator-ui-components/src/components/WfoSubscription/utils/utils.tsx
@@ -86,11 +86,12 @@ export const getWorkflowTargetColor = (
     // Data returned from graphql can't always be depended on to be lowercase
     switch (workflowTarget.toLocaleLowerCase()) {
         case WorkflowTarget.CREATE:
+            return theme.colors.successText;
         case WorkflowTarget.MODIFY:
+        case WorkflowTarget.UPGRADE:
             return theme.colors.primaryText;
         case WorkflowTarget.SYSTEM:
             return theme.colors.warning;
-
         case WorkflowTarget.TERMINATE:
             return theme.colors.danger;
     }
