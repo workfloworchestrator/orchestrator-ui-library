4f10a2201e9ab56549393a338094b2c07cfa51bd
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-10-30 15:44:40 +0100

    Renames constants.ts into configuration.ts and moves orchestrator config to this file

--- apps/wfo-ui/pages/_app.tsx
+++ apps/wfo-ui/pages/_app.tsx
@@ -6,9 +6,6 @@ import { EuiProvider } from '@elastic/eui';
 import {
     ApiClientContextProvider,
     defaultOrchestratorTheme,
-    Environment,
-    getNumberValueFromEnvironmentVariable,
-    OrchestratorConfig,
     OrchestratorConfigProvider,
     ToastsContextProvider,
     ToastsList,
@@ -19,17 +16,11 @@ import '@elastic/eui/dist/eui_theme_light.min.css';
 import { getAppLogo } from '../components/AppLogo/AppLogo';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import {
-    ENGINE_STATUS_ENDPOINT,
-    GRAPHQL_ENDPOINT_CORE,
-    ORCHESTRATOR_API_BASE_URL,
-    PROCESS_DETAIL_DEFAULT_REFETCH_INTERVAL,
-    PROCESS_STATUS_COUNTS_ENDPOINT,
-    SUBSCRIPTION_ACTIONS_ENDPOINT,
-    SUBSCRIPTION_PROCESSES_ENDPOINT,
-} from '../constants';
+    initialOrchestratorConfig,
+    orchestratorApiBaseUrl,
+} from '../configuration';
 import { NextAdapter } from 'next-query-params';
 import { QueryParamProvider } from 'use-query-params';
-import * as process from 'process';
 import { QueryClientConfig } from 'react-query/types/core/types';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { TranslationsProvider } from '../translations/translationsProvider';
@@ -43,38 +34,6 @@ const queryClientConfig: QueryClientConfig = {
             refetchOnWindowFocus: true,
             keepPreviousData: true,
         },
-    },
-};
-
-const orchestratorApiBaseUrl =
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_API_HOST &&
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_API_PATH
-        ? `${process.env.NEXT_PUBLIC_ORCHESTRATOR_API_HOST}${process.env.NEXT_PUBLIC_ORCHESTRATOR_API_PATH}`
-        : ORCHESTRATOR_API_BASE_URL;
-
-const orchestratorGraphqlBaseUrl =
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_HOST &&
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_PATH
-        ? `${process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_HOST}${process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_PATH}`
-        : GRAPHQL_ENDPOINT_CORE;
-
-const initialOrchestratorConfig: OrchestratorConfig = {
-    orchestratorApiBaseUrl,
-    engineStatusEndpoint: orchestratorApiBaseUrl + ENGINE_STATUS_ENDPOINT,
-    processStatusCountsEndpoint:
-        orchestratorApiBaseUrl + PROCESS_STATUS_COUNTS_ENDPOINT,
-    graphqlEndpointCore: orchestratorGraphqlBaseUrl,
-    environmentName:
-        process.env.NEXT_PUBLIC_ENVIRONMENT_NAME ?? Environment.DEVELOPMENT,
-    subscriptionActionsEndpoint:
-        orchestratorApiBaseUrl + SUBSCRIPTION_ACTIONS_ENDPOINT,
-    subscriptionProcessesEndpoint:
-        orchestratorApiBaseUrl + SUBSCRIPTION_PROCESSES_ENDPOINT,
-    dataRefetchInterval: {
-        processDetail: getNumberValueFromEnvironmentVariable(
-            process.env.NEXT_PUBLIC_PROCESS_DETAIL_REFETCH_INTERVAL,
-            PROCESS_DETAIL_DEFAULT_REFETCH_INTERVAL,
-        ),
     },
 };
 
