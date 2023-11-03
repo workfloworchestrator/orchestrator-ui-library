0cdf41a0d164459dc529ce3e35ac8b7027f8b765
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-11-03 14:29:38 +0100

    422 Loads config from environment variables on first page load

--- apps/wfo-ui/configuration.ts
+++ apps/wfo-ui/configuration.ts
@@ -16,34 +16,35 @@ export const SUBSCRIPTION_PROCESSES_ENDPOINT =
 
 export const PROCESS_DETAIL_DEFAULT_REFETCH_INTERVAL = 3000;
 
-export const orchestratorApiBaseUrl =
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_API_HOST &&
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_API_PATH
-        ? `${process.env.NEXT_PUBLIC_ORCHESTRATOR_API_HOST}${process.env.NEXT_PUBLIC_ORCHESTRATOR_API_PATH}`
-        : ORCHESTRATOR_API_BASE_URL;
+export const getInitialOrchestratorConfig = (): OrchestratorConfig => {
+    const orchestratorGraphqlBaseUrl =
+        process.env.ORCHESTRATOR_GRAPHQL_HOST &&
+        process.env.ORCHESTRATOR_GRAPHQL_PATH
+            ? `${process.env.ORCHESTRATOR_GRAPHQL_HOST}${process.env.ORCHESTRATOR_GRAPHQL_PATH}`
+            : GRAPHQL_ENDPOINT_CORE;
 
-export const orchestratorGraphqlBaseUrl =
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_HOST &&
-    process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_PATH
-        ? `${process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_HOST}${process.env.NEXT_PUBLIC_ORCHESTRATOR_GRAPHQL_PATH}`
-        : GRAPHQL_ENDPOINT_CORE;
+    const orchestratorApiBaseUrl =
+        process.env.ORCHESTRATOR_API_HOST && process.env.ORCHESTRATOR_API_PATH
+            ? `${process.env.ORCHESTRATOR_API_HOST}${process.env.ORCHESTRATOR_API_PATH}`
+            : ORCHESTRATOR_API_BASE_URL;
 
-export const initialOrchestratorConfig: OrchestratorConfig = {
-    orchestratorApiBaseUrl,
-    engineStatusEndpoint: orchestratorApiBaseUrl + ENGINE_STATUS_ENDPOINT,
-    processStatusCountsEndpoint:
-        orchestratorApiBaseUrl + PROCESS_STATUS_COUNTS_ENDPOINT,
-    graphqlEndpointCore: orchestratorGraphqlBaseUrl,
-    environmentName:
-        process.env.NEXT_PUBLIC_ENVIRONMENT_NAME ?? Environment.DEVELOPMENT,
-    subscriptionActionsEndpoint:
-        orchestratorApiBaseUrl + SUBSCRIPTION_ACTIONS_ENDPOINT,
-    subscriptionProcessesEndpoint:
-        orchestratorApiBaseUrl + SUBSCRIPTION_PROCESSES_ENDPOINT,
-    dataRefetchInterval: {
-        processDetail: getNumberValueFromEnvironmentVariable(
-            process.env.NEXT_PUBLIC_PROCESS_DETAIL_REFETCH_INTERVAL,
-            PROCESS_DETAIL_DEFAULT_REFETCH_INTERVAL,
-        ),
-    },
+    return {
+        orchestratorApiBaseUrl,
+        engineStatusEndpoint: orchestratorApiBaseUrl + ENGINE_STATUS_ENDPOINT,
+        processStatusCountsEndpoint:
+            orchestratorApiBaseUrl + PROCESS_STATUS_COUNTS_ENDPOINT,
+        graphqlEndpointCore: orchestratorGraphqlBaseUrl,
+        environmentName:
+            process.env.ENVIRONMENT_NAME ?? Environment.DEVELOPMENT,
+        subscriptionActionsEndpoint:
+            orchestratorApiBaseUrl + SUBSCRIPTION_ACTIONS_ENDPOINT,
+        subscriptionProcessesEndpoint:
+            orchestratorApiBaseUrl + SUBSCRIPTION_PROCESSES_ENDPOINT,
+        dataRefetchInterval: {
+            processDetail: getNumberValueFromEnvironmentVariable(
+                process.env.PROCESS_DETAIL_REFETCH_INTERVAL,
+                PROCESS_DETAIL_DEFAULT_REFETCH_INTERVAL,
+            ),
+        },
+    };
 };
