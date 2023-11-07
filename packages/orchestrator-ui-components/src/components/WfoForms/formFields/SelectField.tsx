5e04af70cbb5ad1aba14ef826caad21782cc4fd2
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-11-07 16:30:27 +0100

    337: Use react-select in select component. Single select only

--- packages/orchestrator-ui-components/src/components/WfoForms/formFields/SelectField.tsx
+++ packages/orchestrator-ui-components/src/components/WfoForms/formFields/SelectField.tsx
@@ -14,30 +14,91 @@
  */
 import React from 'react';
 
-import { EuiFormRow, EuiSelect, EuiText } from '@elastic/eui';
-import { connectField, filterDOMProps } from 'uniforms';
+import { EuiFormRow, EuiText } from '@elastic/eui';
+import { get } from 'lodash';
+import { useTranslations } from 'next-intl';
+import ReactSelect from 'react-select';
+import {
+    connectField,
+    filterDOMProps,
+    joinName,
+    useField,
+    useForm,
+} from 'uniforms';
 
-import { FieldProps } from '../../../types/forms';
+import type { EuiThemeComputed } from '@elastic/eui';
+import { ListField, ListFieldProps } from './ListField';
+import { ListItemField } from './ListItemField';
+
+import { FieldProps, Option } from './types';
+import { useOrchestratorTheme } from '../../../hooks';
+
+// Avoid circular deps
+// import { default as ListSelectField } from "lib/uniforms-surfnet/src/ListSelectField";
 
 export type SelectFieldProps = FieldProps<
     string | string[],
-    {
-        allowedValues?: string[];
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        transform: (value: string) => string;
-    }
+    { allowedValues?: string[]; transform?(value: string): string }
 >;
+
+export const getReactSelectInnerComponentStyles = (theme: EuiThemeComputed) => {
+    const reactSelectInnerComponentStyles = {
+        option: (
+            baseStyles: object,
+            state: { isSelected: boolean; isDisabled: boolean },
+        ) => ({
+            ...baseStyles,
+            borderBottom: theme.border.thin,
+            backgroundColor: theme.colors.lightestShade,
+            color: state.isSelected
+                ? theme.colors.primaryText
+                : theme.colors.text,
+            cursor: state.isDisabled ? 'not-allowed' : 'default',
+        }),
+        control: (baseStyles: object) => ({
+            ...baseStyles,
+            backgroundColor: theme.colors.lightestShade,
+            color: theme.colors.text,
+            border: theme.border.thin,
+        }),
+        input: (baseStyles: object) => ({
+            ...baseStyles,
+            color: theme.colors.text,
+        }),
+        singleValue: (baseStyles: object, state: { isDisabled: boolean }) => {
+            const opacity = state.isDisabled ? 0.3 : 1;
+            const transition = 'opacity 300ms';
+            return {
+                ...baseStyles,
+                opacity,
+                transition,
+                color: theme.colors.text,
+            };
+        },
+        menu: (baseStyles: object) => ({
+            ...baseStyles,
+            backgroundColor: theme.colors.lightestShade,
+        }),
+    };
+
+    return {
+        reactSelectInnerComponentStyles,
+    };
+};
 
 function Select({
     allowedValues = [],
     disabled,
+    fieldType,
     id,
+    inputRef,
     label,
     description,
     name,
     onChange,
     placeholder,
     readOnly,
+    required,
     transform,
     value,
     error,
@@ -45,6 +106,34 @@ function Select({
     errorMessage,
     ...props
 }: SelectFieldProps) {
+    const t = useTranslations('pydanticForms');
+    const { theme } = useOrchestratorTheme();
+    const nameArray = joinName(null, name);
+    let parentName = joinName(nameArray.slice(0, -1));
+
+    // We can't call useField conditionally so we call it for ourselves if there is no parent
+    if (parentName === '') {
+        parentName = name;
+    }
+    const parent = useField(parentName, {}, { absoluteName: true })[0];
+    const { model } = useForm();
+
+    if (parentName !== name) {
+        if (
+            parent.fieldType === Array &&
+            (parent as ListFieldProps).uniqueItems
+        ) {
+            const allValues: string[] = get(model, parentName, []);
+            const chosenValues = allValues.filter(
+                (_item, index) =>
+                    index.toString() !== nameArray[nameArray.length - 1],
+            );
+
+            allowedValues = allowedValues.filter(
+                (value) => !chosenValues.includes(value),
+            );
+        }
+    }
     const options = allowedValues.map((value: string) => ({
         label: transform ? transform(value) : value,
         text: transform ? transform(value) : value,
@@ -52,37 +141,60 @@ function Select({
     }));
 
     const selectedValue = options.find(
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        (option: any) => option.value === value,
+        (option: Option) => option.value === value,
     );
 
-    return (
-        <section {...filterDOMProps(props)}>
-            <EuiFormRow
-                label={label}
-                labelAppend={<EuiText size="m">{description}</EuiText>}
-                error={showInlineError ? errorMessage : false}
-                isInvalid={error}
-                id={id}
-                fullWidth
-            >
-                <EuiSelect
+    // React select allows callbacks to supply style for innercomponents: https://react-select.com/styles#inner-components
+    const { reactSelectInnerComponentStyles } =
+        getReactSelectInnerComponentStyles(theme);
+
+    if (fieldType === Array) {
+        console.log('Selectfield type is array');
+        // Avoid circular import with our own ListSelectField (instead of recursively trying to use SelectField)
+        return <div>Fieldtype array</div>;
+        /*
+        return (
+            <ListField name={name}>
+                <ListItemField name="$">
+                    <ListSelectField name="" transform={transform} allowedValues={allowedValues} />
+                </ListItemField>
+            </ListField>
+        );
+        */
+    } else {
+        return (
+            <section {...filterDOMProps(props)}>
+                <EuiFormRow
+                    label={label}
+                    labelAppend={<EuiText size="m">{description}</EuiText>}
+                    error={showInlineError ? errorMessage : false}
+                    isInvalid={error}
                     id={id}
-                    name={name}
-                    onChange={(option) => {
-                        if (!readOnly) {
-                            onChange(option?.target.value);
+                    fullWidth
+                >
+                    <ReactSelect<Option, false>
+                        id={id}
+                        inputId={`${id}.search`}
+                        name={name}
+                        onChange={(option) => {
+                            if (!readOnly) {
+                                onChange(option?.value);
+                            }
+                        }}
+                        styles={reactSelectInnerComponentStyles}
+                        options={options}
+                        value={selectedValue}
+                        isSearchable={true}
+                        isClearable={true}
+                        placeholder={
+                            placeholder || t('widgets.select.placeholder')
                         }
-                    }}
-                    options={options}
-                    value={selectedValue?.value}
-                    fullWidth={true}
-                    placeholder={placeholder}
-                    disabled={disabled || readOnly}
-                />
-            </EuiFormRow>
-        </section>
-    );
+                        isDisabled={disabled || readOnly}
+                    />
+                </EuiFormRow>
+            </section>
+        );
+    }
 }
 
 export const SelectField = connectField(Select);
