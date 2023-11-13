11c710a07eabee38ee656e019fbd57d7798d9ae6
tjeerddie <tjeerdverschragen@hotmail.com>
2023-11-13 16:31:58 +0100

    Fix AUTH_ACTIVE not correctly turning off authentication (#446)
    * Fix AUTH_ACTIVE not correctly turning off authentication
    
    * 234 - Moves ConfigProvider to top and outside NoSSR. Uses the config from provider to determine authentication is enabled.
    
    * 234 - Adds logging for Okteto debugging
    
    * 234 - Adds logging for Okteto debugging pt2
    
    * 234 - Adds logging for Okteto debugging pt3
    
    * 234 - Removes logging
    
    ---------
    
    Co-authored-by: Tjeerd.Verschragen <tjeerd.verschragen@surf.nl>
    Co-authored-by: Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,5 @@
 import NextAuth, { AuthOptions } from 'next-auth';
+import { OAuthConfig } from 'next-auth/providers';
 import { SessionToken } from '@orchestrator-ui/orchestrator-ui-components';
 import { JWT } from 'next-auth/jwt';
 
@@ -6,33 +7,36 @@ const token_endpoint_auth_method = process.env.NEXTAUTH_CLIENT_SECRET
     ? 'client_secret_basic'
     : 'none';
 
+export interface WfoProfile extends Record<string, string> {}
+
+const authActive = process.env.AUTH_ACTIVE?.toLowerCase() != 'false';
+const WfoProvider: OAuthConfig<WfoProfile> = {
+    id: process.env.NEXTAUTH_ID || '',
+    name: process.env.NEXTAUTH_ID || '',
+    clientId: process.env.NEXTAUTH_CLIENT_ID || '',
+    clientSecret: process.env.NEXTAUTH_CLIENT_SECRET || undefined,
+    wellKnown: `${
+        process.env.NEXTAUTH_ISSUER || ''
+    }/.well-known/openid-configuration`,
+    type: 'oauth',
+    authorization: { params: { scope: 'openid profile' } },
+    checks: ['pkce', 'state'],
+    idToken: true,
+    profile(profile) {
+        return {
+            id: profile.sub,
+            name: profile.name ?? profile.preferred_username,
+            email: profile.email,
+        };
+    },
+    client: {
+        token_endpoint_auth_method,
+        response_types: ['code'],
+    },
+};
+
 export const authOptions: AuthOptions = {
-    providers: [
-        {
-            id: process.env.NEXTAUTH_ID || '',
-            name: process.env.NEXTAUTH_ID || '',
-            clientId: process.env.NEXTAUTH_CLIENT_ID || '',
-            clientSecret: process.env.NEXTAUTH_CLIENT_SECRET || undefined,
-            wellKnown: `${
-                process.env.NEXTAUTH_ISSUER || ''
-            }/.well-known/openid-configuration`,
-            type: 'oauth',
-            authorization: { params: { scope: 'openid profile' } },
-            checks: ['pkce', 'state'],
-            idToken: true,
-            profile(profile) {
-                return {
-                    id: profile.sub,
-                    name: profile.name ?? profile.preferred_username,
-                    email: profile.email,
-                };
-            },
-            client: {
-                token_endpoint_auth_method,
-                response_types: ['code'],
-            },
-        },
-    ],
+    providers: authActive ? [WfoProvider] : [],
     callbacks: {
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
