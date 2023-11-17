659b4ee80208089ae5b0b038ca920352936ec4b6
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-11-17 15:20:07 +0100

    414-sort-imports: Add sorting plugin and configuration for import sort rule

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -12,10 +12,30 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 'error',
         'react/react-in-jsx-scope': 2,
         'react/jsx-uses-react': 2,
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal'],
+                pathGroups: [
+                    {
+                        pattern: 'react',
+                        group: 'external',
+                        position: 'before',
+                    },
+                ],
+                pathGroupsExcludedImportTypes: ['react'],
+                'newlines-between': 'always',
+                alphabetize: {
+                    order: 'asc',
+                    caseInsensitive: true,
+                },
+            },
+        ],
     },
     parserOptions: {
         babelOptions: {
             presets: [require.resolve('next/babel')],
         },
     },
+    plugins: ['eslint-plugin-import'],
 };
