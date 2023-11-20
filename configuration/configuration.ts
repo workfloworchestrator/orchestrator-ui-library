5354a4bdfea5693a639af4739e0f6a65172d8bbf
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-11-20 14:49:33 +0100

    342 - Implements the service call to delete a process

--- configuration/configuration.ts
+++ configuration/configuration.ts
@@ -11,6 +11,7 @@ export const DEFAULT_ORCHESTRATOR_API_BASE_URL = 'http://localhost:8080/api';
 
 export const ENGINE_STATUS_ENDPOINT = '/settings/status';
 export const PROCESS_STATUS_COUNTS_ENDPOINT = '/processes/status-counts';
+export const PROCESSES_ENDPOINT = '/processes';
 export const SUBSCRIPTION_ACTIONS_ENDPOINT = '/subscriptions/workflows';
 export const SUBSCRIPTION_PROCESSES_ENDPOINT =
     '/processes/process-subscriptions-by-subscription-id';
@@ -32,9 +33,10 @@ export const getInitialOrchestratorConfig = (): OrchestratorConfig => {
     return {
         orchestratorApiBaseUrl,
         engineStatusEndpoint: orchestratorApiBaseUrl + ENGINE_STATUS_ENDPOINT,
+        graphqlEndpointCore: orchestratorGraphqlBaseUrl,
         processStatusCountsEndpoint:
             orchestratorApiBaseUrl + PROCESS_STATUS_COUNTS_ENDPOINT,
-        graphqlEndpointCore: orchestratorGraphqlBaseUrl,
+        processesEndpoint: orchestratorApiBaseUrl + PROCESSES_ENDPOINT,
         environmentName:
             process.env.ENVIRONMENT_NAME ?? Environment.DEVELOPMENT,
         subscriptionActionsEndpoint:
