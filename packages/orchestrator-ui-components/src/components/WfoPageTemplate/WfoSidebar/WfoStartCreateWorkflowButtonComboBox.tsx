74bc9b73a0410290414d218502e9f476a290881b
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-11-28 21:46:46 +0100

    415 Applies isEngineRunning check to the new workflow buttons

--- packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/WfoStartCreateWorkflowButtonComboBox.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/WfoStartCreateWorkflowButtonComboBox.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next/router';
 
+import { ToastTypes } from '@/contexts';
 import { GET_WORKFLOWS_FOR_DROPDOWN_LIST_GRAPHQL_QUERY } from '@/graphqlQueries/workflows/workflowsQueryForDropdownList';
-import { useQueryWithGraphql } from '@/hooks';
+import {
+    useEngineStatusQuery,
+    useQueryWithGraphql,
+    useToastMessage,
+} from '@/hooks';
 
 import {
     WfoButtonComboBox,
@@ -15,6 +20,9 @@ import { PATH_START_NEW_PROCESS } from '../paths';
 export const WfoStartCreateWorkflowButtonComboBox = () => {
     const router = useRouter();
     const t = useTranslations('common');
+    const tErrors = useTranslations('errors');
+    const { isEngineRunningNow } = useEngineStatusQuery();
+    const toastMessage = useToastMessage();
 
     const { data } = useQueryWithGraphql(
         GET_WORKFLOWS_FOR_DROPDOWN_LIST_GRAPHQL_QUERY,
@@ -40,7 +48,19 @@ export const WfoStartCreateWorkflowButtonComboBox = () => {
             )
             .sort((a, b) => a.label.localeCompare(b.label)) ?? [];
 
-    const handleOptionChange = (selectedProduct: WorkflowComboBoxOption) => {
+    const handleOptionChange = async (
+        selectedProduct: WorkflowComboBoxOption,
+    ) => {
+        const isEngineRunning = await isEngineRunningNow();
+
+        if (!isEngineRunning) {
+            return toastMessage.addToast(
+                ToastTypes.ERROR,
+                tErrors('notAllowedWhenEngineIsNotRunningMessage'),
+                tErrors('notAllowedWhenEngineIsNotRunningTitle'),
+            );
+        }
+
         const { workflowName, productId } = selectedProduct.data;
         router.push({
             pathname: `${PATH_START_NEW_PROCESS}/${workflowName}`,
