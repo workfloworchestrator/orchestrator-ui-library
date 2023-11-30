3d9ec0ca18cf3983c3cd7f54e2e5ebe833fb2168
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-11-30 13:21:21 +0100

    445: Use useQeuryWithFetch instead of axios client

--- packages/orchestrator-ui-components/src/messages/useGetTranslationMessages.ts
+++ packages/orchestrator-ui-components/src/messages/useGetTranslationMessages.ts
@@ -1,34 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useContext } from 'react';
 
 import { AbstractIntlMessages } from 'next-intl';
 
-import { useAxiosApiClient } from '../components/WfoForms/useAxiosApiClient';
-import { Locale } from '../types';
+import { OrchestratorConfigContext } from '@/contexts';
+import { useQueryWithFetch } from '@/hooks';
+import { Locale } from '@/types';
+
 import enGB from './en-GB.json';
 import nlNL from './nl-NL.json';
 
 export type TranslationMessagesMap = Map<Locale, AbstractIntlMessages>;
 
-export const useGetTranslationMessages = (locale: string | undefined) => {
-    const apiClient = useAxiosApiClient();
-    const [backendMessages, setBackendMessages] = useState<
-        Record<string, string>
-    >({});
+type BackendTranslationsResponse = {
+    forms: {
+        fields: Record<string, string>;
+    };
+};
 
-    useEffect(() => {
-        apiClient
-            .axiosFetch<{ forms: { fields: Record<string, string> } }>(
-                `translations/${locale}`,
-            )
-            .then((response) => {
-                const backendMessages = response.forms.fields;
-                setBackendMessages(backendMessages);
-            })
-            .catch((error) => {
-                console.error('error getting backend translations', error);
-                setBackendMessages({});
-            });
-    }, [apiClient, locale]);
+export const useGetTranslationMessages = (locale: string | undefined) => {
+    const { orchestratorApiBaseUrl } = useContext(OrchestratorConfigContext);
+    const translationsUrl = `${orchestratorApiBaseUrl}/translations/${locale}`;
+    const { isLoading, data } = useQueryWithFetch<
+        BackendTranslationsResponse,
+        Record<never, never>
+    >(translationsUrl, {}, 'backendTranslations');
+
+    const backendMessages = isLoading ? {} : data?.forms?.fields || {};
 
     const getLocalMessages = () => {
         switch (locale) {
