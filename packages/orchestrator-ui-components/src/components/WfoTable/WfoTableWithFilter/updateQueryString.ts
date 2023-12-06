ca510af79a38d63e206369154118c71cdb2282e3
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2023-12-06 11:59:33 +0100

    478 Updates regex to find fields in the queryString

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoTableWithFilter/updateQueryString.ts
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoTableWithFilter/updateQueryString.ts
@@ -13,40 +13,46 @@ const toQueryValue = (value: string) => {
     return isContainingSpaces(value) ? `"${value}"` : value;
 };
 
-// Three cases to consider when this function is called
-// 1 - queryString is empty
-// 2 - fieldName already exists in query string
+// Four cases to consider when this function is called
+// 1 - fieldName or value is empty
+// 2 - queryString is empty
 // 3 - fieldName does not exist in query string
+// 4 - fieldName already exists in query string
 export const updateQueryString = (
-    queryString: string | undefined,
+    queryString: string,
     fieldName: string,
     value: string,
 ) => {
-    // 1 - Empty query string returning directly fieldName:value
+    // 1 - Returning the exising query string when either fieldName or value is empty
+    if (fieldName === '' || value === '') {
+        return queryString;
+    }
+
+    // 2 - Empty query string returning directly fieldName:value
     if (!queryString) {
         return `${fieldName}:${toQueryValue(value)}`;
     }
 
+    // Finding fieldName with values in queryString in this order:
+    // - Value is not surrounded with anything
+    // field:value
+    // - Value is surrounded with " and " (spaces are allowed)
+    // field:"value with spaces"
+    // - Value is surrounded with ( and )
+    // field:(value1|value2)
+    // field:("value1 with spaces"|value2|value3)
     const fieldRegex = new RegExp(
-        `${fieldName}:(\\([\\S "]+\\)|"[\\S ]+"|\\S+)`,
+        `${fieldName}:(\\w+|"[\\w ]+"|\\([\\w"| ]+\\))`,
     );
     const match = queryString.match(fieldRegex);
 
-    if (match) {
-        // 2 - Field name already exists in query string as:
-        // field:value
-        // field:"value with spaces"
-        // field:(value1|value2)
-        // field:("value1 with spaces"|value2|value3)
-        const existingValue = getValueWithoutBrackets(match[1]);
-        const updatedValue = `${existingValue}|${toQueryValue(value)}`;
-
-        return queryString.replace(
-            fieldRegex,
-            `${fieldName}:(${updatedValue})`,
-        );
+    // 3 - Field name does not exist in query string yet, appending fieldName:value
+    if (!match) {
+        return `${queryString} ${fieldName}:${toQueryValue(value)}`;
     }
 
-    // 3 - Field name does not exist in query string yet, appending fieldName:value
-    return `${queryString} ${fieldName}:${toQueryValue(value)}`;
+    // 4 - Field name already exists in query string as:
+    const existingValue = getValueWithoutBrackets(match[1]);
+    const updatedValue = `${existingValue}|${toQueryValue(value)}`;
+    return queryString.replace(fieldRegex, `${fieldName}:(${updatedValue})`);
 };
