4bdf5eda92456aeb6b200aa5724682914c62f4ca
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-12-13 14:16:06 +0100

    385: Readd icons used in tabs

--- packages/orchestrator-ui-components/src/components/WfoSubscription/subscriptionDetailTabs.tsx
+++ packages/orchestrator-ui-components/src/components/WfoSubscription/subscriptionDetailTabs.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import { EuiIcon } from '@elastic/eui';
+
 import { WfoFilterTab } from '../WfoFilterTabs';
 import { WfoSubscriptionDetailTab } from './utils';
 
@@ -6,23 +10,21 @@ export const subscriptionDetailTabs: WfoFilterTab<WfoSubscriptionDetailTab>[] =
         {
             id: WfoSubscriptionDetailTab.GENERAL_TAB,
             translationKey: 'general',
+            prepend: <EuiIcon type="devToolsApp" />,
         },
         {
             id: WfoSubscriptionDetailTab.SERVICE_CONFIGURATION_TAB,
             translationKey: 'serviceConfiguration',
+            prepend: <EuiIcon type="submodule" />,
         },
         {
             id: WfoSubscriptionDetailTab.PROCESSES_TAB,
             translationKey: 'workflows',
+            prepend: <EuiIcon type="indexRuntime" />,
         },
         {
             id: WfoSubscriptionDetailTab.RELATED_SUBSCRIPTIONS_TAB,
             translationKey: 'relatedSubscriptions',
+            prepend: <EuiIcon type="heatmap" />,
         },
     ];
-/*
-  -        prepend: <EuiIcon type="devToolsApp" />,
-  -        prepend: <EuiIcon type="submodule" />,
-  -        prepend: <EuiIcon type="indexRuntime" />,
-  -        prepend: <EuiIcon type="heatmap" />,
-*/
