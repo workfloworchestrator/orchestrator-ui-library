e43f8a268b576835e017ed31e9b861f5849d219f
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-12-19 11:58:30 +0100

    577: Remove options object from useSession hoook since its not used and might be confusing

--- packages/orchestrator-ui-components/src/hooks/useSessionWithToken.ts
+++ packages/orchestrator-ui-components/src/hooks/useSessionWithToken.ts
@@ -1,14 +1,12 @@
 import { Session } from 'next-auth';
-import { UseSessionOptions, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 
 export type SessionToken = Session & {
     accessToken?: string;
 };
 
-export const useSessionWithToken = (
-    options?: UseSessionOptions<boolean> | undefined,
-) => {
-    const sessionData = useSession(options);
+export const useSessionWithToken = () => {
+    const sessionData = useSession();
     const dataWithToken = sessionData.data as SessionToken;
     return { ...sessionData, session: dataWithToken };
 };
