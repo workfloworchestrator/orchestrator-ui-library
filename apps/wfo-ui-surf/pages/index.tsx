71c8c74e39de39216cfb9c5d213b9c0138816fb5
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2023-12-20 12:54:38 +0100

    579: Add retrieving name from session on startup page to surf app

--- apps/wfo-ui-surf/pages/index.tsx
+++ apps/wfo-ui-surf/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { useSession } from 'next-auth/react';
 import { useTranslations } from 'next-intl';
 
 import { EuiPageHeader, EuiSpacer } from '@elastic/eui';
@@ -10,11 +11,12 @@ import {
 } from '@orchestrator-ui/orchestrator-ui-components';
 
 export function Index() {
+    const { data: session } = useSession();
     const t = useTranslations('main');
+    const username = session?.user?.name || '';
     return (
         <>
-            <EuiPageHeader pageTitle={`${t('welcome')} Georgi`} />
-
+            <EuiPageHeader pageTitle={`${t('welcome')} ${username}`} />
             <EuiSpacer />
             <WfoNewProcessPanel />
             <EuiSpacer />
