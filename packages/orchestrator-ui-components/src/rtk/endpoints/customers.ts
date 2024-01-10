9339131c95df2f5d7e9fd9b879a764026992e9d7
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-01-10 15:29:07 +0100

    RTK-POC: Moce customer query to rtk

---
+++ packages/orchestrator-ui-components/src/rtk/endpoints/customers.ts
@@ -0,0 +1,27 @@
+import { Customer, CustomersResult } from '@/types';
+
+import { orchestratorApi } from './../api';
+
+const customersQuery = `query Customers {
+  customers(first: 1000000, after: 0) {
+      page {
+          fullname
+          identifier
+          shortcode
+      }
+  }
+}`;
+
+const customersApi = orchestratorApi.injectEndpoints({
+    endpoints: (build) => ({
+        getCustomers: build.query<Customer[], void>({
+            query: () => ({ document: customersQuery }),
+            transformResponse: (response: CustomersResult): Customer[] => {
+                const customers = response?.customers?.page || [];
+                return customers;
+            },
+        }),
+    }),
+});
+
+export const { useGetCustomersQuery } = customersApi;
