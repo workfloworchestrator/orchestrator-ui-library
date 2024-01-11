d92f62829d9dae65303fffc7a528a80c14b7c7ba
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-01-11 12:37:20 +0100

    RTK-POC: Toggle between graphql and rest version of baseQuery

--- packages/orchestrator-ui-components/src/rtk/api.ts
+++ packages/orchestrator-ui-components/src/rtk/api.ts
@@ -1,11 +1,34 @@
-import { createApi } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query';
+
+export enum BaseQueryTypes {
+    fetch = 'fetch',
+    graphql = 'graphql',
+    custom = 'custom',
+}
+
+type ExtraOptions = {
+    baseQueryType?: BaseQueryTypes;
+};
 
 export const orchestratorApi = createApi({
     reducerPath: 'orchestratorApi',
-    baseQuery: graphqlRequestBaseQuery({
-        // baseUrl: 'https://orchestrator.dev.automation.surf.net',
-        url: 'https://orchestrator.dev.automation.surf.net/api/graphql',
-    }),
+    baseQuery: (args, api, extraOptions: ExtraOptions) => {
+        const { baseQueryType } = extraOptions || {};
+
+        switch (baseQueryType) {
+            case BaseQueryTypes.fetch:
+                const fetchFn = fetchBaseQuery({
+                    baseUrl: 'https://orchestrator.dev.automation.surf.net/api',
+                });
+                return fetchFn(args, api, {});
+            default:
+                const graphqlFn = graphqlRequestBaseQuery({
+                    // baseUrl: 'https://orchestrator.dev.automation.surf.net',
+                    url: 'https://orchestrator.dev.automation.surf.net/api/graphql',
+                });
+                return graphqlFn(args, api, {});
+        }
+    },
     endpoints: () => ({}),
 });
