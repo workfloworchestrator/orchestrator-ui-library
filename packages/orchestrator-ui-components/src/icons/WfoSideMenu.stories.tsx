20db1c7819916dd0905a4b3c44b27d73f3ae28e7
Wouter Huisman <wouter.huisman@surf.nl>
2024-01-16 16:34:35 +0100

    fix storybook

---
+++ packages/orchestrator-ui-components/src/icons/WfoSideMenu.stories.tsx
@@ -0,0 +1,13 @@
+import type { Meta } from '@storybook/react';
+
+import { WfoSideMenu } from './WfoSidemenu';
+
+const Story: Meta<typeof WfoSideMenu> = {
+    component: WfoSideMenu,
+    title: 'Icons/WfoSideMenu',
+};
+export default Story;
+
+export const Primary = {
+    args: {},
+};
