f85215b58b60a3f64ca798fa84299ff88b9be8ac
Georgi Manev <61829525+Georgi2704@users.noreply.github.com>
2024-01-25 17:21:09 +0100

    576 align icons to design style (#696)
    * some extra icons
    
    * subscription tab uses new icons
    
    * delete the newly added unused icons
    
    * Fixed colors when switching tabs + added properties for fixing position of icon
    
    * linted
    
    * Fixed black icons for WfoStep and moved currentColor to subscriptionDetailTabs
    
    * Fixed imports
    
    * Reverted position props, added EuiFlexGroup to center the icons
    
    * Fix for icon title, id
    
    * Correct title for WfoCubeSolid
    
    ---------
    
    Co-authored-by: Wouter Huisman <wouter.huisman@surf.nl>

--- packages/orchestrator-ui-components/src/components/WfoSubscription/subscriptionDetailTabs.tsx
+++ packages/orchestrator-ui-components/src/components/WfoSubscription/subscriptionDetailTabs.tsx
@@ -1,29 +1,59 @@
 import React from 'react';
 
-import { EuiIcon } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
-import { WfoFilterTab } from '../WfoFilterTabs';
+import { WfoFilterTab } from '@/components';
+import { WfoCogFill, WfoCubeSolid, WfoPlayCircle, WfoShare } from '@/icons';
+
 import { SubscriptionDetailTab } from './utils';
 
 export const subscriptionDetailTabs: WfoFilterTab<SubscriptionDetailTab>[] = [
     {
-        id: SubscriptionDetailTab.SERVICE_CONFIGURATION_TAB,
-        translationKey: 'serviceConfiguration',
-        prepend: <EuiIcon type="submodule" />,
-    },
-    {
         id: SubscriptionDetailTab.GENERAL_TAB,
         translationKey: 'general',
-        prepend: <EuiIcon type="devToolsApp" />,
+        prepend: (
+            <EuiFlexGroup justifyContent={'center'}>
+                <EuiFlexItem>
+                    <WfoCubeSolid width="18" height="18" color="currentColor" />
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        ),
+    },
+    {
+        id: SubscriptionDetailTab.SERVICE_CONFIGURATION_TAB,
+        translationKey: 'serviceConfiguration',
+        prepend: (
+            <EuiFlexGroup justifyContent={'center'}>
+                <EuiFlexItem>
+                    <WfoCogFill width="16" height="16" color="currentColor" />
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        ),
     },
     {
         id: SubscriptionDetailTab.PROCESSES_TAB,
         translationKey: 'workflows',
-        prepend: <EuiIcon type="indexRuntime" />,
+        prepend: (
+            <EuiFlexGroup justifyContent={'center'}>
+                <EuiFlexItem>
+                    <WfoPlayCircle
+                        width="18"
+                        height="18"
+                        color="currentColor"
+                    />
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        ),
     },
     {
         id: SubscriptionDetailTab.RELATED_SUBSCRIPTIONS_TAB,
         translationKey: 'relatedSubscriptions',
-        prepend: <EuiIcon type="heatmap" />,
+        prepend: (
+            <EuiFlexGroup justifyContent={'center'}>
+                <EuiFlexItem>
+                    <WfoShare width="16" height="16" color="currentColor" />
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        ),
     },
 ];
---
+++ packages/orchestrator-ui-components/src/icons/WfoCubeSolid.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+
+import { WfoIconProps } from './WfoIconProps';
+
+export const WfoCubeSolid: FC<WfoIconProps> = ({
+    width = 24,
+    height = 24,
+    color = '#000000',
+}) => (
+    <svg
+        width={width}
+        height={height}
+        viewBox={`0 0 24 24`}
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <title>icon/cube-solid</title>
+        <g
+            id="Symbols"
+            stroke="none"
+            strokeWidth="1"
+            fill="none"
+            fillRule="evenodd"
+        >
+            <g id="icon/cube-solid" fill={color} fillRule="nonzero">
+                <path
+                    d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z"
+                    id="Combined-Shape"
+                ></path>
+            </g>
+        </g>
+    </svg>
+);
