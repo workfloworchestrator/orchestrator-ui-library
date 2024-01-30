d503a54e49192e5fcaaeda4c6e67b87d066cc748
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-01-30 14:47:57 +0100

    647-split wfo-ui app into own repo

--- apps/wfo-ui-surf/pages/service-tickets/[serviceTicketId]/new-email.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { WfoServiceTicketNewEmailPage } from '../../../components/WfoServiceTicketsNewEmailPage/WfoServiceTicketNewEmailPage';
-
-export const ServiceTicketDetailPage = () => <WfoServiceTicketNewEmailPage />;
-
-export default ServiceTicketDetailPage;
