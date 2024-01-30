1cad137629a6a35ec7a86d73f10eb5e87b3b9bd5
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-01-30 14:43:25 +0100

    647-split wfo-ui app into own repo

--- apps/wfo-ui/pages/subscriptions/[subscriptionId].tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { WfoSubscriptionDetailPage } from '@orchestrator-ui/orchestrator-ui-components';
-
-const SubscriptionDetailPage = () => <WfoSubscriptionDetailPage />;
-
-export default SubscriptionDetailPage;
--- apps/wfo-ui/pages/tasks/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { WfoTasksListPage } from '@orchestrator-ui/orchestrator-ui-components';
-
-export function TasksPage() {
-    return <WfoTasksListPage />;
-}
-
-export default TasksPage;
