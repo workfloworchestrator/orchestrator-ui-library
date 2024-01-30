f9718bf380f279bab3e2724479456b1eebeee9fa
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-01-30 14:47:57 +0100

    647-split wfo-ui app into own repo

--- apps/wfo-ui-surf/pages/metadata/productblocks.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { WfoProductBlocksPage } from '@orchestrator-ui/orchestrator-ui-components';
-
-export const ProductBlocksPage = () => <WfoProductBlocksPage />;
-
-export default ProductBlocksPage;
--- apps/wfo-ui-surf/pages/service-tickets/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import { WfoServiceTicketListPage } from '../../components/WfoServiceTicketsListPage';
-
-export const ActiveServiceTicketsPage = () => <WfoServiceTicketListPage />;
-
-export default ActiveServiceTicketsPage;
