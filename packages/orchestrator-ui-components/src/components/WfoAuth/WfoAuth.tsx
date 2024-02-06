133af6aa6ee9cd1b56fac1c9c3ec5467581a62c2
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-02-06 17:24:42 +0100

    460 Sets up oidc with a wellknown config. Adds OPA proof of concept

--- packages/orchestrator-ui-components/src/components/WfoAuth/WfoAuth.tsx
+++ packages/orchestrator-ui-components/src/components/WfoAuth/WfoAuth.tsx
@@ -1,9 +1,10 @@
 import React, { JSX, useContext } from 'react';
 
-import { useSession } from 'next-auth/react';
+import { loadPolicy } from '@open-policy-agent/opa-wasm';
 
 import { WfoLoading } from '@/components';
 import { OrchestratorConfigContext } from '@/contexts';
+import { useWfoSession } from '@/hooks';
 
 interface AuthProps {
     children: JSX.Element;
@@ -11,8 +12,37 @@ interface AuthProps {
 
 export const WfoAuth = ({ children }: AuthProps): JSX.Element => {
     const { authActive } = useContext(OrchestratorConfigContext);
-    const { status } = useSession({
+    const { status, session } = useWfoSession({
         required: authActive,
+    });
+
+    // TODO: Can only do this when logged in with OIDC
+    const getPolicy = async () => {
+        const policyResult = await fetch('https://[public-bundle]/[clientId]');
+        const policyWasm = await policyResult.arrayBuffer();
+        return await loadPolicy(policyWasm);
+    };
+
+    getPolicy().then((loadedPolicy) => {
+        if (session) {
+            const { profile } = session;
+
+            const policyInput = {
+                resource: '/orchestrator/prefixes/',
+                active: true,
+                // client_id: 'orchestrator-client.test.automation.surf.net', // TEST
+                client_id: 'orchestrator-client.automation.surf.net', // Production
+                method: 'GET',
+                ...profile,
+            };
+            const policyTestResult = loadedPolicy.evaluate(policyInput);
+            console.error('Policy test', {
+                policyResult: policyTestResult[0].result,
+                policyTestResult,
+                policyInput,
+                profile,
+            });
+        }
     });
 
     if (status === 'loading') {
