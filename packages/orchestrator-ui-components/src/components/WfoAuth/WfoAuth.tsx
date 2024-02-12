fe6353937af5bf435772cd317acf50f010ecc90d
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-02-12 12:40:40 +0100

    460 Replaces JSX.Element with ReactNode

--- packages/orchestrator-ui-components/src/components/WfoAuth/WfoAuth.tsx
+++ packages/orchestrator-ui-components/src/components/WfoAuth/WfoAuth.tsx
@@ -1,4 +1,4 @@
-import React, { JSX, useContext } from 'react';
+import React, { ReactNode, useContext } from 'react';
 
 import { WfoLoading } from '@/components';
 import { OrchestratorConfigContext } from '@/contexts';
@@ -6,14 +6,30 @@ import { PolicyContextProvider } from '@/contexts/PolicyContext';
 import { useWfoSession } from '@/hooks';
 
 interface AuthProps {
-    children: JSX.Element;
+    children: ReactNode;
     isAllowed?: (routerPath: string, resource?: string) => boolean;
 }
 
-export const WfoAuth = ({
-    children,
-    isAllowed = () => true,
-}: AuthProps): JSX.Element => {
+/**
+ * The WfoAuth component exposes a function isAllowed to apply policy checks.
+ * In components in this library a usePolicy hook is available that exposes this function
+ * with the optional parameter "resource".
+ *
+ * For convenience there is also a WfoIsAllowedToRender component available. In both hook
+ * and component the current route is determined in the usePolicy hook and passed as
+ * first parameter in the isAllowed function in this WfoAuth component.
+ *
+ * Example for usage in any component:
+ * const { isAllowed } = usePolicy();
+ * const isAllowedToDoOrSeeSomething: boolean = isAllowed('something')
+ *
+ * Example for implementing the isAllowed function:
+ * const isAllowed = (routerPath: string, resource?: string) => {
+ *     // Your own rules to determine is something is allowed or not
+ *     // The useWfoSession hook can be used to get the current user profile
+ * }
+ */
+export const WfoAuth = ({ children, isAllowed = () => true }: AuthProps) => {
     const { authActive } = useContext(OrchestratorConfigContext);
     const { status } = useWfoSession({
         required: authActive,
