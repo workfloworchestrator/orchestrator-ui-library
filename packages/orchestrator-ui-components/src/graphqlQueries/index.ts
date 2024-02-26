8b9d4651ae7020b83cb0a6cbdfa7ca3e5df2969e
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-02-26 15:29:56 +0100

    702 Removes processDetailQuery from library

--- packages/orchestrator-ui-components/src/graphqlQueries/index.ts
+++ packages/orchestrator-ui-components/src/graphqlQueries/index.ts
@@ -3,7 +3,6 @@ export * from './productBlocksQuery';
 export * from './productsQuery';
 export * from './resourceTypesQuery';
 export * from './subscriptionDetailQuery';
-export * from './processDetailQuery';
 export * from './subscriptionsDropdownOptionsQuery';
 export * from './processStepsQuery';
 export * from './processListQuery';
