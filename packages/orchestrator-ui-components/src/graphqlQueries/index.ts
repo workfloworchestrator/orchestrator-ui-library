7275c2c016b214069a4d07b3abdf70768fee5f19
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-03-08 14:25:49 +0100

    702: Remove old processStepsQeury tile

--- packages/orchestrator-ui-components/src/graphqlQueries/index.ts
+++ packages/orchestrator-ui-components/src/graphqlQueries/index.ts
@@ -4,5 +4,4 @@ export * from './productsQuery';
 export * from './resourceTypesQuery';
 export * from './subscriptionDetailQuery';
 export * from './subscriptionsDropdownOptionsQuery';
-export * from './processStepsQuery';
 export * from './processListQuery';
