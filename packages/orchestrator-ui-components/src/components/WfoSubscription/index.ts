fc655f7d6d1b0890ca6c7706685aea0c296068aa
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-03-11 15:46:43 +0100

    702 Adds subscriptionDetail endpoint to RTK Client

--- packages/orchestrator-ui-components/src/components/WfoSubscription/index.ts
+++ packages/orchestrator-ui-components/src/components/WfoSubscription/index.ts
@@ -1,9 +1,11 @@
 export * from './utils';
-export * from './WfoProcessesTimeline';
-export * from './WfoSubscriptionProductBlock';
-export * from './WfoSubscriptionActions';
+
 export * from './SubscriptionKeyValueBlock';
+export * from './WfoInSyncField';
+export * from './WfoProcessesTimeline';
+export * from './WfoRelatedSubscriptions';
+export * from './WfoSubscription';
+export * from './WfoSubscriptionActions';
+export * from './WfoSubscriptionProductBlock';
 export * from './WfoSubscriptionDetailTree';
 export * from './WfoSubscriptionGeneral';
-export * from './WfoSubscription';
-export * from './WfoInSyncField';
---
+++ packages/orchestrator-ui-components/src/rtk/endpoints/subscriptionDetail.ts
@@ -0,0 +1,97 @@
+import { CacheTags, orchestratorApi } from '@/rtk';
+import {
+    BaseGraphQlResult,
+    SubscriptionDetail,
+    SubscriptionDetailResult,
+} from '@/types';
+
+export const subscriptionDetailQuery = `
+    query SubscriptionDetail($subscriptionId: String!) {
+        subscriptions(
+            filterBy: { value: $subscriptionId, field: "subscriptionId" }
+        ) {
+            page {
+                subscriptionId
+                description
+                fixedInputs
+                insync
+                note
+                product {
+                    createdAt
+                    name
+                    status
+                    endDate
+                    description
+                    tag
+                    productType
+                    productId
+                }
+                endDate
+                startDate
+                status
+                customerId
+                customer {
+                    fullname
+                    customerId
+                    shortcode
+                }
+                productBlockInstances {
+                    id
+                    ownerSubscriptionId
+                    parent
+                    productBlockInstanceValues
+                    subscriptionInstanceId
+                    inUseByRelations
+                }
+                processes(sortBy: { field: "startedAt", order: ASC }) {
+                    page {
+                        processId
+                        lastStatus
+                        startedAt
+                        createdBy
+                        workflowTarget
+                        workflowName
+                        isTask
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export type SubscriptionDetailResponse = {
+    subscriptions: SubscriptionDetail[];
+} & BaseGraphQlResult;
+
+const subscriptionDetailApi = orchestratorApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getSubscriptionDetail: builder.query<
+            SubscriptionDetailResponse,
+            { subscriptionId: string }
+        >({
+            query: (variables) => ({
+                document: subscriptionDetailQuery,
+                variables,
+            }),
+            transformResponse: (
+                response: SubscriptionDetailResult,
+            ): SubscriptionDetailResponse => {
+                const subscriptions = response.subscriptions.page || [];
+                const pageInfo = response.subscriptions.pageInfo || {};
+
+                return {
+                    subscriptions,
+                    pageInfo,
+                };
+            },
+            providesTags: (result, error, arg) => [
+                {
+                    type: CacheTags.subscription,
+                    id: arg.subscriptionId,
+                },
+            ],
+        }),
+    }),
+});
+
+export const { useGetSubscriptionDetailQuery } = subscriptionDetailApi;
