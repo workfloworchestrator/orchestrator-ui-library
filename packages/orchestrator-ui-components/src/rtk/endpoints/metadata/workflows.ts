62ec8c0bf7a39a1cf71d41bae8b3f61ac14ba9ec
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-03-12 16:25:58 +0100

    702: Add metadata workflowsApi

---
+++ packages/orchestrator-ui-components/src/rtk/endpoints/metadata/workflows.ts
@@ -0,0 +1,73 @@
+import { orchestratorApi } from '@/rtk';
+import {
+    BaseGraphQlResult,
+    GraphqlQueryVariables,
+    WorkflowDefinition,
+    WorkflowDefinitionsResult,
+} from '@/types';
+
+export const workflowsQuery = `
+query MetadataWorkflows(
+    $first: Int!
+    $after: Int!
+    $sortBy: [GraphqlSort!]
+    $query: String
+) {
+    workflows(
+        first: $first
+        after: $after
+        sortBy: $sortBy
+        query: $query
+    ) {
+        page {
+            name
+            description
+            target
+            products {
+                tag
+            }
+            createdAt
+        }
+        pageInfo {
+            endCursor
+            hasNextPage
+            hasPreviousPage
+            startCursor
+            totalItems
+            sortFields
+            filterFields
+        }
+    }
+}
+`;
+
+export type WorkflowsResponse = {
+    workflows: WorkflowDefinition[];
+} & BaseGraphQlResult;
+
+const workflowsApi = orchestratorApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getWorkflows: builder.query<
+            WorkflowsResponse,
+            GraphqlQueryVariables<WorkflowDefinition>
+        >({
+            query: (variables) => ({
+                document: workflowsQuery,
+                variables,
+            }),
+            transformResponse: (
+                response: WorkflowDefinitionsResult,
+            ): WorkflowsResponse => {
+                const workflows = response.workflows.page || [];
+                const pageInfo = response.workflows.pageInfo || {};
+
+                return {
+                    workflows,
+                    pageInfo,
+                };
+            },
+        }),
+    }),
+});
+
+export const { useGetWorkflowsQuery, useLazyGetWorkflowsQuery } = workflowsApi;
