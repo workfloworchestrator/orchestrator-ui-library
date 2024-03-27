63edcf161371c0821cb26f3a510ffe99cee234db
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-03-27 17:05:27 +0100

    102 Introduces hook to get the config from the store and expose a getOverriddenValue function

--- packages/orchestrator-ui-components/src/contexts/index.ts
+++ packages/orchestrator-ui-components/src/contexts/index.ts
@@ -3,4 +3,3 @@ export * from './ConfirmationDialogProvider';
 export * from './OrchestratorConfigContext';
 export * from './PolicyContext';
 export * from './TreeContext';
-export * from './ValueOverrideContext';
