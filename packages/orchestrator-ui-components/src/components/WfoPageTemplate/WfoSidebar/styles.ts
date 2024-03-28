d4b14692ff83115d098ea450cef00aca16e09b6d
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-03-28 13:14:15 +0100

    905: Improve sideMenu design. Add selected styles from sketch

--- packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/styles.ts
+++ packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/styles.ts
@@ -15,3 +15,30 @@ export const getCopyrightStyles = (theme: EuiThemeComputed) => {
         copyrightStyle,
     };
 };
+
+export const getMenuItemStyles = (theme: EuiThemeComputed) => {
+    const menuItemStyle = css({
+        color: theme.colors.title,
+        padding: `${theme.base * 0.625}px ${theme.base * 0.75}px`,
+        display: 'flex',
+        alignItems: 'center',
+        ':hover': {
+            textDecoration: 'underline',
+        },
+    });
+
+    const selectedMenuItem = css({
+        height: `${theme.base * 2.25}px`,
+        backgroundColor: theme.colors.lightShade,
+        borderRadius: theme.border.radius.medium,
+        padding: `${theme.base * 0.5}px ${theme.base * 0.75}px`,
+        display: 'flex',
+        alignItems: 'center',
+        fontWeight: theme.font.weight.semiBold,
+    });
+
+    return {
+        menuItemStyle,
+        selectedMenuItem,
+    };
+};
