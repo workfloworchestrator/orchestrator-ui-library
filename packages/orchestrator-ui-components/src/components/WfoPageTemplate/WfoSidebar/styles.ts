a50a9f6e8e54f35e53ce7e3b932e54f90d48b9a9
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-03-28 14:36:50 +0100

    905: Add submenu styling

--- packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/styles.ts
+++ packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoSidebar/styles.ts
@@ -1,5 +1,5 @@
 import { EuiThemeComputed } from '@elastic/eui';
-import { css } from '@emotion/react';
+import { CSSObject, css } from '@emotion/react';
 
 export const getCopyrightStyles = (theme: EuiThemeComputed) => {
     const copyrightStyle = css({
@@ -17,28 +17,62 @@ export const getCopyrightStyles = (theme: EuiThemeComputed) => {
 };
 
 export const getMenuItemStyles = (theme: EuiThemeComputed) => {
-    const menuItemStyle = css({
-        color: theme.colors.title,
-        padding: `${theme.base * 0.625}px ${theme.base * 0.75}px`,
+    const baseStyles: CSSObject = {
+        lineHeight: `${theme.base * 1.25}px`,
         display: 'flex',
         alignItems: 'center',
         ':hover': {
             textDecoration: 'underline',
         },
+        color: theme.colors.text,
+        padding: `${theme.base * 0.5}px ${theme.base * 0.75}px`,
+    };
+
+    const baseSubMenuStyles: CSSObject = {
+        ...baseStyles,
+        ':after': {
+            content: "''",
+            top: '16px',
+            left: 0,
+            width: '4px',
+            height: '1px',
+            backgroundColor: '#d3dae6',
+            position: 'absolute',
+        },
+        padding: '8px 12px',
+        ':last-child': {
+            top: '-4px',
+            position: 'relative',
+        },
+    };
+
+    const menuItemStyle = css({
+        ...baseStyles,
+        color: theme.colors.title,
     });
 
     const selectedMenuItem = css({
+        ...baseStyles,
         height: `${theme.base * 2.25}px`,
         backgroundColor: theme.colors.lightShade,
         borderRadius: theme.border.radius.medium,
-        padding: `${theme.base * 0.5}px ${theme.base * 0.75}px`,
-        display: 'flex',
-        alignItems: 'center',
         fontWeight: theme.font.weight.semiBold,
+        color: theme.colors.primaryText,
+    });
+
+    const selectedSubMenuItem = css({
+        ...baseSubMenuStyles,
+        fontWeight: theme.font.weight.semiBold,
+    });
+
+    const subMenuItemStyle = css({
+        ...baseSubMenuStyles,
     });
 
     return {
         menuItemStyle,
         selectedMenuItem,
+        selectedSubMenuItem,
+        subMenuItemStyle,
     };
 };
