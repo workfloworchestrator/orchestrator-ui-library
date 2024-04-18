49c53516c1a585b7cb883fe19ed4e164bce04e5c
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-04-18 13:55:39 +0200

    776: Add inUseByRelations RTK hook

---
+++ packages/orchestrator-ui-components/src/rtk/endpoints/subscriptionInUseByRelationsList.ts
@@ -0,0 +1,54 @@
+import { CacheTags, orchestratorApi } from '@/rtk';
+import { InUseByRelationDetail, InUseByRelationsDetailResult } from '@/types';
+
+export const subscriptionInUseByRelationQuery = `
+    query SubscriptionsInUseByRelationsDetails(
+        $subscriptionIds: String!
+    ) {
+        subscriptions(
+            first: 1000000
+            after: 0
+            filterBy: [{field: "subscriptionId", value: $subscriptionIds},{field: "status", value: "INITIAL|ACTIVE|MIGRATING|DISABLED|PROVISIONING"}]
+          ) {
+            page {
+              product {
+                productType
+              }
+              description
+              subscriptionId
+              status
+            }
+          }
+    }
+`;
+
+export type InUseByRelationsDetailResponse = {
+    inUseByRelationDetails: InUseByRelationDetail[];
+};
+
+const subscriptionInUseByRelationsListApi = orchestratorApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getInUseByRelationDetails: builder.query<
+            InUseByRelationsDetailResponse,
+            { subscriptionIds: string }
+        >({
+            query: (variables) => ({
+                document: subscriptionInUseByRelationQuery,
+                variables,
+            }),
+            transformResponse: (
+                response: InUseByRelationsDetailResult,
+            ): InUseByRelationsDetailResponse => {
+                const subscriptions = response.subscriptions.page || [];
+
+                return {
+                    inUseByRelationDetails: subscriptions,
+                };
+            },
+            providesTags: [CacheTags.subscriptionList],
+        }),
+    }),
+});
+
+export const { useGetInUseByRelationDetailsQuery } =
+    subscriptionInUseByRelationsListApi;
