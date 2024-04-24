2490e6d915bd25fb06b7c504b4bd55c5154cf0f1
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-04-24 16:31:27 +0200

    626 Page Header: updates color of the pipe between the app logo and environment badge

--- packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoPageHeader/styles.ts
+++ packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoPageHeader/styles.ts
@@ -2,18 +2,13 @@ import { css } from '@emotion/react';
 
 import { WfoTheme } from '@/hooks';
 
-import { toHexColorWithOpacity } from './utils/toHexColorWithOpacity';
-
 export const getWfoPageHeaderStyles = ({ theme }: WfoTheme) => {
     const navigationBackgroundColor = '#04385F';
 
     const appNameStyle = css({
         paddingRight: theme.base,
         marginRight: theme.base,
-        borderRight: `${theme.border.width.thick} solid ${toHexColorWithOpacity(
-            theme.colors.emptyShade,
-            0.3,
-        )}`,
+        borderRight: `${theme.border.width.thick} solid ${theme.colors.subduedText}`,
     });
 
     const getHeaderStyle = (navigationHeight: number) => {
