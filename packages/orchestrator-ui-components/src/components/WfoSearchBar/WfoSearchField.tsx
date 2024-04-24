5cc898032e90ff17cf6975dabb7f25b3621b2994
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-04-24 16:15:48 +0200

    626 WfoSearchField: Adds style overrides to implement dark theme

--- packages/orchestrator-ui-components/src/components/WfoSearchBar/WfoSearchField.tsx
+++ packages/orchestrator-ui-components/src/components/WfoSearchBar/WfoSearchField.tsx
@@ -4,6 +4,10 @@ import { useTranslations } from 'next-intl';
 
 import { EuiFieldSearch, EuiFormRow } from '@elastic/eui';
 
+import { useWithOrchestratorTheme } from '@/hooks';
+
+import { getWfoSearchBarStyles } from './styles';
+
 export type WfoSearchFieldProps = {
     queryString?: string;
     onUpdateQueryString?: (queryString: string) => void;
@@ -15,6 +19,7 @@ export const WfoSearchField = ({
 }: WfoSearchFieldProps) => {
     const t = useTranslations('common');
     const tError = useTranslations('errors');
+    const { searchBarStyle } = useWithOrchestratorTheme(getWfoSearchBarStyles);
 
     const queryIsValid = true; // Query validation turned of for now until ESQueries can be sent to the backend
 
@@ -33,6 +38,7 @@ export const WfoSearchField = ({
             error={[tError('invalidQueryParts')]}
         >
             <EuiFieldSearch
+                css={searchBarStyle}
                 value={currentQuery}
                 placeholder={`${t('search')}...`}
                 onChange={(event) => setCurrentQuery(event.target.value)}
