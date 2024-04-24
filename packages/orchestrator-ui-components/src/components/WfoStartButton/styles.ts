57ce6eea72e2d7249500ae3fb58811efe49909ad
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2024-04-24 17:30:36 +0200

    626 WfoStartButtonCombobox: specifies styles for dark theme

--- packages/orchestrator-ui-components/src/components/WfoStartButton/styles.ts
+++ packages/orchestrator-ui-components/src/components/WfoStartButton/styles.ts
@@ -1,10 +1,24 @@
 import { css } from '@emotion/react';
 
-export const getStyles = () => {
+import { WfoTheme } from '@/hooks';
+
+export const getStyles = ({ theme }: WfoTheme) => {
     const comboBoxWidth = '300px';
 
     const selectableStyle = css({
         width: comboBoxWidth,
+
+        '.euiFieldSearch': {
+            backgroundColor: theme.colors.body,
+            color: theme.colors.text,
+            '&:focus': {
+                backgroundColor: theme.colors.emptyShade,
+            },
+        },
+
+        '.euiSelectableList .euiSelectableListItem': {
+            borderColor: theme.colors.lightShade,
+        },
     });
 
     return {
