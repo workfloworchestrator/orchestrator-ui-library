d019ce18dc78558887c241bfa88c9ac45ce5c51e
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-06-13 14:55:27 +0200

    1163: Add test and fix test setup

--- packages/orchestrator-ui-components/jest.config.cjs
+++ packages/orchestrator-ui-components/jest.config.cjs
@@ -1,6 +1,17 @@
 const base = require('@orchestrator-ui/jest-config/jest-base.config.js');
+const nextJest = require('next/jest');
 
-module.exports = {
+const createJestConfig = nextJest({
+    // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+    dir: './',
+});
+
+// The entry for "uuid" in the moduleNameMapper can be removed when EUI updates the dependency version to 9.0.0 or higher.
+// https://github.com/uuidjs/uuid/blob/main/CHANGELOG.md#900-2022-09-05
+const customJestConfig = {
     ...base,
-    displayName: 'Orchestrator UI Components Tests',
+    displayName: 'Wfo-UI Tests',
+    moduleNameMapper: { '^uuid$': 'uuid' },
 };
+
+module.exports = createJestConfig(customJestConfig);
