851eb96baf32b8f690de790835b37528be5aa1da
Georgi Manev <61829525+Georgi2704@users.noreply.github.com>
2024-09-18 09:41:50 +0200

    Fix workflow dropdown not refreshed after subscription is set in sync (#1426)
    * Fix workflow dropdown not getting refreshed after subscription is set in sync
    
    * Added changeset

--- packages/orchestrator-ui-components/src/rtk/endpoints/subscriptionActions.ts
+++ packages/orchestrator-ui-components/src/rtk/endpoints/subscriptionActions.ts
@@ -1,6 +1,7 @@
 import { SUBSCRIPTION_ACTIONS_ENDPOINT } from '@/configuration';
 import { BaseQueryTypes, orchestratorApi } from '@/rtk';
-import { SubscriptionActions } from '@/types';
+import { CacheTagType, SubscriptionActions } from '@/types';
+import { getCacheTag } from '@/utils/cacheTag';
 
 const subscriptionActionsApi = orchestratorApi.injectEndpoints({
     endpoints: (build) => ({
@@ -12,6 +13,15 @@ const subscriptionActionsApi = orchestratorApi.injectEndpoints({
                 `${SUBSCRIPTION_ACTIONS_ENDPOINT}/${subscriptionId}`,
             extraOptions: {
                 baseQueryType: BaseQueryTypes.fetch,
+            },
+            providesTags: (result, error, queryArguments) => {
+                if (!error && result) {
+                    return getCacheTag(
+                        CacheTagType.subscriptions,
+                        queryArguments.subscriptionId,
+                    );
+                }
+                return [];
             },
         }),
     }),
