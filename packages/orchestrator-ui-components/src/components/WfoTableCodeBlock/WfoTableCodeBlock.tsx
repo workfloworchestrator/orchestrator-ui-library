614735dbf3be7e8fc0d09db79e1e5e23c365dd93
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2024-11-11 13:08:02 +0100

    1506: Converts stepdata to key values, adds some styling

--- packages/orchestrator-ui-components/src/components/WfoTableCodeBlock/WfoTableCodeBlock.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTableCodeBlock/WfoTableCodeBlock.tsx
@@ -3,24 +3,31 @@ import React, { FC } from 'react';
 import { EuiFlexItem } from '@elastic/eui';
 
 import { useWithOrchestratorTheme } from '@/hooks';
+import type { StepState } from '@/types';
 
 import { WfoKeyValueTable } from '../WfoKeyValueTable';
 import type { WfoKeyValueTableDataType } from '../WfoKeyValueTable';
 import { getStyles } from './styles';
 
 export type WfoTableCodeBlockProps = {
-    data: object;
+    data: StepState;
 };
 
 export const WfoTableCodeBlock: FC<WfoTableCodeBlockProps> = ({ data }) => {
-    console.log(data);
-
     const { tableCodeBlockMarginStyle } = useWithOrchestratorTheme(getStyles);
 
-    const keyValues: WfoKeyValueTableDataType[] = [
-        { key: 'key1', value: 'value1', textToCopy: 'textToCopy1' },
-        { key: 'key2', value: 'value2' },
-    ];
+    const keyValues: WfoKeyValueTableDataType[] = Object.entries(data).map(
+        (entry) => {
+            const key = entry[0];
+            const value = entry[1] as string;
+
+            return {
+                key,
+                value,
+                textToCopy: value,
+            };
+        },
+    );
 
     return (
         <EuiFlexItem css={tableCodeBlockMarginStyle}>
--- packages/orchestrator-ui-components/src/components/WfoTableCodeBlock/styles.ts
+++ packages/orchestrator-ui-components/src/components/WfoTableCodeBlock/styles.ts
@@ -1,12 +1,10 @@
 import { css } from '@emotion/react';
 
-import { WfoTheme } from '@/hooks';
-
-export const getStyles = ({ theme }: WfoTheme) => {
+export const getStyles = () => {
     const tableCodeBlockMarginStyle = css({
-        margin: 10,
-        borderRadius: theme.border.radius.medium,
-        border: `thin solid ${theme.colors.lightestShade}`,
+        margin: 15,
+        marginTop: 20,
+        marginRight: 20,
     });
 
     return {
