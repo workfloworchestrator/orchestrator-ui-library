1beed56a4f664e0a4e3c01a49911433af672ffb8
Ricardo van der Heijden <20791917+ricardovdheijden@users.noreply.github.com>
2025-01-02 21:20:24 +0800

    1479 Adds sync version of the csvDownloadHandler function

--- packages/orchestrator-ui-components/src/utils/csvDownload.ts
+++ packages/orchestrator-ui-components/src/utils/csvDownload.ts
@@ -47,8 +47,8 @@ export const getCsvFileNameWithDate = (fileNameWithoutExtension: string) => {
     return `${fileNameWithoutExtension}_${year}-${month}-${day}-${hour}${minute}${second}.csv`;
 };
 
-export const csvDownloadHandler = <T extends object, U extends object>(
-    dataFetchFunction: () => Promise<T | undefined>,
+export const csvDownloadHandlerSync = <T extends object, U extends object>(
+    data: T,
     dataMapper: (data: T) => U[],
     pageInfoMapper: (data: T) => GraphQLPageInfo,
     keyOrder: string[],
@@ -59,29 +59,67 @@ export const csvDownloadHandler = <T extends object, U extends object>(
         variables?: TranslationValues,
     ) => string,
 ) => {
-    return async () => {
+    const dataForExport = dataMapper(data).map((dataRow) =>
+        toObjectWithSortedKeys(toObjectWithSerializedValues(dataRow), keyOrder),
+    );
+
+    const pageInfo = pageInfoMapper(data);
+    if ((pageInfo.totalItems ?? 0) > MAXIMUM_ITEMS_FOR_BULK_FETCHING) {
+        addToastFunction(
+            ToastTypes.ERROR,
+            translationFunction('notAllResultsExported', {
+                totalResults: pageInfo.totalItems,
+                maximumExportedResults: MAXIMUM_ITEMS_FOR_BULK_FETCHING,
+            }),
+            translationFunction('notAllResultsExportedTitle'),
+        );
+    }
+    initiateCsvFileDownload(dataForExport, filename);
+};
+
+export const csvDownloadHandler =
+    <T extends object, U extends object>(
+        dataFetchFunction: () => Promise<T | undefined>,
+        dataMapper: (data: T) => U[],
+        pageInfoMapper: (data: T) => GraphQLPageInfo,
+        keyOrder: string[],
+        filename: string,
+        addToastFunction: (
+            type: ToastTypes,
+            text: string,
+            title: string,
+        ) => void,
+        translationFunction: (
+            translationKey: string,
+            variables?: TranslationValues,
+        ) => string,
+    ) =>
+    async () => {
         const data: T | undefined = await dataFetchFunction();
 
         if (data) {
-            const dataForExport = dataMapper(data).map((dataRow) =>
-                toObjectWithSortedKeys(
-                    toObjectWithSerializedValues(dataRow),
-                    keyOrder,
-                ),
+            csvDownloadHandlerSync(
+                data,
+                dataMapper,
+                pageInfoMapper,
+                keyOrder,
+                filename,
+                addToastFunction,
+                translationFunction,
             );
-
-            const pageInfo = pageInfoMapper(data);
-            if ((pageInfo.totalItems ?? 0) > MAXIMUM_ITEMS_FOR_BULK_FETCHING) {
-                addToastFunction(
-                    ToastTypes.ERROR,
-                    translationFunction('notAllResultsExported', {
-                        totalResults: pageInfo.totalItems,
-                        maximumExportedResults: MAXIMUM_ITEMS_FOR_BULK_FETCHING,
-                    }),
-                    translationFunction('notAllResultsExportedTitle'),
-                );
-            }
-            initiateCsvFileDownload(dataForExport, filename);
         }
     };
-};
+
+export const getPageInfoForSyncExport = (
+    dataLength: number,
+    sortFields?: string[],
+    filterFields?: string[],
+): GraphQLPageInfo => ({
+    startCursor: 0,
+    endCursor: Math.max(0, dataLength - 1),
+    hasNextPage: false,
+    totalItems: dataLength,
+    hasPreviousPage: false,
+    sortFields: sortFields ?? [],
+    filterFields: filterFields ?? [],
+});
