1bb8c3c280bed2233280e1ac8b019609be3e1b9a
Alex Ray <19390913+Sparrow1029@users.noreply.github.com>
2025-02-13 14:28:20 -0700

    DONT MERGE: 1727 Allow adding more version info to hover modal (#1733)
    * Add icon, gql query, sub context menu
    
    * Add orchestrator-ui-components npm pkg version to popover
    
    * Change icon
    
    * Add changeset
    
    * Add translations
    
    * Break up nested ternary; address PR comments
    
    * Small change to gql query for version endpoint

--- packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoPageHeader/WfoHamburgerMenu.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPageTemplate/WfoPageHeader/WfoHamburgerMenu.tsx
@@ -5,14 +5,18 @@ import { useTranslations } from 'next-intl';
 
 import { EuiButtonIcon, EuiContextMenu, EuiPopover } from '@elastic/eui';
 import type { EuiContextMenuPanelDescriptor } from '@elastic/eui';
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace';
 
+import { WfoLoading } from '@/components/WfoLoading';
+import { ORCHESTRATOR_UI_LIBRARY_VERSION } from '@/configuration';
 import { useGetOrchestratorConfig, useOrchestratorTheme } from '@/hooks';
-import { WfoLogoutIcon } from '@/icons';
+import { WfoLogoutIcon, WfoSquareStack3dStack, WfoXCircleFill } from '@/icons';
 import { WfoQuestionCircle } from '@/icons/WfoQuestionCircle';
+import { useGetVersionsQuery } from '@/rtk/endpoints/versions';
 import { toOptionalArrayEntry } from '@/utils';
 
 export const WfoHamburgerMenu = ({}) => {
-    const t = useTranslations('main');
+    const t = useTranslations('hamburgerMenu');
     const [isPopoverOpen, setPopoverIsOpen] = useState(false);
     const { theme, isDarkThemeActive } = useOrchestratorTheme();
     const { enableSupportMenuItem, supportMenuItemUrl } =
@@ -23,9 +27,50 @@ export const WfoHamburgerMenu = ({}) => {
     const handleOpenSupport = async (): Promise<undefined> => {
         window.open(supportMenuItemUrl, '_blank');
     };
+    const {
+        data,
+        isFetching,
+        error: versionFetchError,
+    } = useGetVersionsQuery();
+
+    const getVersionItems = () => {
+        if (versionFetchError) {
+            console.error(versionFetchError);
+            return [
+                {
+                    name: 'Error fetching application version data',
+                    icon: <WfoXCircleFill color={theme.colors.danger} />,
+                },
+            ];
+        }
+        if (isFetching) {
+            return [{ name: '', icon: <WfoLoading /> }];
+        }
+        // initial array contains orchestrator-ui-components library version
+        const versionsArr: [{ name: string; icon: EmotionJSX.Element }] = [
+            {
+                name: `orchestrator-ui-components: ${ORCHESTRATOR_UI_LIBRARY_VERSION}`,
+                icon: <WfoSquareStack3dStack />,
+            },
+        ];
+
+        if (data === undefined) {
+            return versionsArr;
+        }
+
+        const orchApiVersions = data.version.applicationVersions.map((item) => {
+            return {
+                name: item,
+                icon: <WfoSquareStack3dStack />,
+            };
+        });
+
+        // orchestrator-ui-components library version + versions returned from orchestrator api
+        return versionsArr.concat(orchApiVersions);
+    };
 
     const logoutItem = {
-        name: 'Logout',
+        name: t('logout'),
         icon: (
             <WfoLogoutIcon
                 color={
@@ -37,7 +82,7 @@ export const WfoHamburgerMenu = ({}) => {
     };
 
     const supportItem = {
-        name: 'Support',
+        name: t('support'),
         icon: (
             <WfoQuestionCircle
                 color={
@@ -48,8 +93,15 @@ export const WfoHamburgerMenu = ({}) => {
         onClick: handleOpenSupport,
     };
 
+    const versionItem = {
+        name: t('softwareVersions'),
+        icon: <WfoSquareStack3dStack width={24} height={24} />,
+        panel: 1,
+    };
+
     const panelItems = [
         ...toOptionalArrayEntry(supportItem, enableSupportMenuItem),
+        versionItem,
         { ...logoutItem },
     ];
 
@@ -57,6 +109,11 @@ export const WfoHamburgerMenu = ({}) => {
         {
             id: 0,
             items: panelItems,
+        },
+        {
+            id: 1,
+            title: t('softwareVersions'),
+            items: getVersionItems(),
         },
     ];
 
@@ -77,7 +134,11 @@ export const WfoHamburgerMenu = ({}) => {
             panelPaddingSize="none"
             anchorPosition="downLeft"
         >
-            <EuiContextMenu initialPanelId={0} panels={panels} />
+            <EuiContextMenu
+                initialPanelId={0}
+                panels={panels}
+                css={{ width: theme.base * 25 }}
+            />
         </EuiPopover>
     );
 };
