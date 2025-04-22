f96e0501274c907fd43f87034da578d58dfb2d1c
Georgi2704 <jrmanev@gmail.com>
2025-04-22 18:37:16 +0200

    1337 Move getWebSocket to utils.ts

--- packages/orchestrator-ui-components/src/rtk/utils.ts
+++ packages/orchestrator-ui-components/src/rtk/utils.ts
@@ -1,9 +1,11 @@
 import { GraphQLError } from 'graphql/error/GraphQLError';
 import { isPlainObject } from 'lodash';
+import { getSession } from 'next-auth/react';
 
 import { SerializedError } from '@reduxjs/toolkit';
 import { FetchBaseQueryError } from '@reduxjs/toolkit/query';
 
+import type { WfoSession } from '@/hooks';
 import { WfoGraphqlError } from '@/rtk/api';
 
 export function stripUndefined(obj: object): Record<string, unknown> {
@@ -76,4 +78,18 @@ export const mapRtkErrorToWfoError = (
         ];
     }
     return error;
+};
+
+export const getWebSocket = async (url: string) => {
+    const session = (await getSession()) as WfoSession;
+
+    if (session?.accessToken) {
+        // Implemented authentication taking this into account: https://stackoverflow.com/questions/4361173/http-headers-in-websockets-client-api/77060459#77060459
+        return new WebSocket(url, [
+            'base64.bearer.token',
+            session?.accessToken,
+        ]);
+    } else {
+        return new WebSocket(url);
+    }
 };
