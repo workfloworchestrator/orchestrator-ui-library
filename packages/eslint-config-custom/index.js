8c752155c7ce3bb458601d67f26261559bdf6a0b
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-04-28 14:02:50 +0200

    1802: Update eslint to 9 branch in shared config and lib

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,21 +1,32 @@
-module.exports = {
-    extends: [
-        'plugin:@typescript-eslint/eslint-plugin/recommended',
-        'next',
-        'next/core-web-vitals',
-        'prettier',
-    ],
-    rules: {
-        '@next/next/no-html-link-for-pages': 'off',
-        '@typescript-eslint/ban-ts-comment': 'warn',
-        '@typescript-eslint/no-explicit-any': 'error',
-        'no-console': ['error', { allow: ['error', 'warn'] }],
-        'react/react-in-jsx-scope': 2,
-        'react/jsx-uses-react': 2,
-    },
-    parserOptions: {
-        babelOptions: {
-            presets: [require.resolve('next/babel')],
+import prettier from 'eslint-config-prettier';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import typescriptEslint from 'typescript-eslint';
+
+import js from '@eslint/js';
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        rules: {
+            'no-console': ['error', { allow: ['error', 'warn'] }],
         },
     },
-};
+    {
+        files: ['**/*.{js,ts,jsx,tsx}'],
+        languageOptions: { globals: globals.browser },
+    },
+    {
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        rules: {
+            'react-hooks/rules-of-hooks': 'error', // Enforce Rules of Hooks
+            'react-hooks/exhaustive-deps': 'warn', // Enforce exhaustive deps in effects
+        },
+    },
+    typescriptEslint.configs.recommended,
+]);
---
+++ packages/orchestrator-ui-components/eslint.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'eslint/config';
+
+import wfoConfig from '@orchestrator-ui/eslint-config-custom';
+
+export default defineConfig([wfoConfig]);
