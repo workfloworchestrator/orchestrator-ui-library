4546eb3201a0be3c0646c3a6f98249e865d710ce
Georgi2704 <jrmanev@gmail.com>
2025-06-03 17:10:27 +0200

    Used correct name for icon + cleanup

--- packages/orchestrator-ui-components/src/icons/WfoExclamationTriangle.tsx
+++ packages/orchestrator-ui-components/src/icons/WfoExclamationTriangle.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 import { WfoIconProps } from './WfoIconProps';
 
-export const WfoExclamationCircle: FC<WfoIconProps> = ({
+export const WfoExclamationTriangle: FC<WfoIconProps> = ({
     width = 24,
     height = 24,
     color = '#000000',
@@ -27,11 +27,3 @@ export const WfoExclamationCircle: FC<WfoIconProps> = ({
         </g>
     </svg>
 );
-//
-// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-//     <path fillRule="evenodd" d="M18 10a8 8 0 1 1-16 0 8 8 0 0 1 16 0Zm-8-5a.75.75 0 0 1 .75.75v4.5a.75.75 0 0 1-1.5 0v-4.5A.75.75 0 0 1 10 5Zm0 10a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
-// </svg>
-//
-// <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-//     <path fillRule="evenodd" d="M8.485 2.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 2.495ZM10 5a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 10 5Zm0 9a1 1 0 1 0 0-2 1 1 0 0 0 0 2Z" clipRule="evenodd" />
-// </svg>
