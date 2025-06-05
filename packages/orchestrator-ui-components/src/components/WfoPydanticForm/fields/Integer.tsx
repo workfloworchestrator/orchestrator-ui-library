799c24d29b598e6506a5d7215d91c55475bae564
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-06-05 15:49:02 +0200

    1904: Adds EUI integer field

---
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/Integer.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import type { PydanticFormControlledElement } from 'pydantic-forms';
+
+import { EuiFieldNumber } from '@elastic/eui';
+import { css } from '@emotion/react';
+
+import type { WfoTheme } from '@/hooks';
+import { useWithOrchestratorTheme } from '@/hooks';
+
+const getFormFieldsBaseStyle = ({ theme }: WfoTheme) => {
+    const formFieldBaseStyle = css({
+        backgroundColor: theme.colors.body,
+        color: theme.colors.text,
+        '&:focus': {
+            backgroundColor: theme.colors.emptyShade,
+        },
+    });
+
+    return {
+        formFieldBaseStyle,
+    };
+};
+
+export const Integer: PydanticFormControlledElement = ({
+    pydanticFormField,
+    onChange,
+    value,
+    disabled,
+}) => {
+    const { formFieldBaseStyle } = useWithOrchestratorTheme(
+        getFormFieldsBaseStyle,
+    );
+
+    return (
+        <EuiFieldNumber
+            css={formFieldBaseStyle}
+            name={pydanticFormField.id}
+            onChange={(event) => onChange(parseInt(event.target.value))}
+            value={value}
+            disabled={disabled}
+        />
+    );
+};
