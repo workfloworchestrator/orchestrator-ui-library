99513d6b176f76c2228e02937b7142272a9657d0
Georgi2704 <jrmanev@gmail.com>
2025-06-10 18:52:59 +0200

    Array field without default array item

---
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoArrayField/WfoArrayField.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { useFieldArray } from 'react-hook-form';
+
+import {
+    PydanticFormElementProps,
+    RenderFields,
+    fieldToComponentMatcher,
+    itemizeArrayItem,
+    usePydanticFormContext,
+} from 'pydantic-forms';
+
+import { EuiIcon } from '@elastic/eui';
+
+import { useOrchestratorTheme } from '@/hooks';
+
+import { getWfoArrayFieldStyles } from './styles';
+
+// adjust relative path accordingly
+
+const MinusButton = ({
+    index,
+    onRemove,
+}: {
+    index: number;
+    onRemove: (index: number) => void;
+}) => {
+    const { theme } = useOrchestratorTheme();
+    const { minusButton } = getWfoArrayFieldStyles();
+
+    return (
+        <span css={minusButton} onClick={() => onRemove(index)}>
+            <EuiIcon type="minus" size="xxl" color={theme.colors.danger} />
+        </span>
+    );
+};
+
+const PlusButton = ({ onClick }: { onClick: () => void }) => {
+    const { theme } = useOrchestratorTheme();
+    const { plusButtonWrapper } = getWfoArrayFieldStyles();
+
+    return (
+        <div css={plusButtonWrapper}>
+            <EuiIcon
+                onClick={onClick}
+                type="plus"
+                size="xxl"
+                color={theme.colors.success}
+            />
+        </div>
+    );
+};
+
+export const WfoArrayField = ({
+    pydanticFormField,
+}: PydanticFormElementProps) => {
+    const { config, rhf } = usePydanticFormContext();
+    const { control } = rhf;
+    const { id: arrayName, arrayItem } = pydanticFormField;
+    const { minItems, maxItems } = pydanticFormField.validations;
+    const { container, fieldWrapper } = getWfoArrayFieldStyles();
+
+    const { fields, append, remove } = useFieldArray({
+        control,
+        name: arrayName,
+    });
+
+    const showMinus = !minItems || fields.length > minItems;
+    const showPlus = !maxItems || fields.length < maxItems;
+
+    if (!arrayItem) return null;
+
+    const component = fieldToComponentMatcher(
+        arrayItem,
+        config?.componentMatcher,
+    );
+
+    const renderField = (field: Record<'id', string>, index: number) => {
+        const arrayField = itemizeArrayItem(index, arrayItem);
+
+        return (
+            <div key={field.id} css={fieldWrapper}>
+                <RenderFields
+                    pydanticFormComponents={[
+                        {
+                            Element: component.Element,
+                            pydanticFormField: arrayField,
+                        },
+                    ]}
+                    extraTriggerFields={[arrayName]}
+                />
+                {showMinus && <MinusButton index={index} onRemove={remove} />}
+            </div>
+        );
+    };
+
+    return (
+        <div css={container}>
+            {fields.map(renderField)}
+
+            {showPlus && (
+                <PlusButton
+                    onClick={() => {
+                        append({
+                            [arrayName]: arrayItem.default ?? undefined,
+                        });
+                    }}
+                />
+            )}
+        </div>
+    );
+};
