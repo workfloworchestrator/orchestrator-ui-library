48fd905faf9d3eb30217cc879d3a82bd5e21e2b0
Karel van Klink <karel.vanklink@geant.org>
2025-07-08 13:25:24 +0200

    Remove abortAllowed from process query, add isAllowed to task creation query

--- packages/orchestrator-ui-components/src/utils/getProductNamesFromProcess.spec.ts
+++ packages/orchestrator-ui-components/src/utils/getProductNamesFromProcess.spec.ts
@@ -14,7 +14,6 @@ const getProcessDetail = (data: Partial<ProcessDetail> = {}): ProcessDetail => {
         traceback: null,
         userPermissions: {
             retryAllowed: true,
-            abortAllowed: true,
             inputAllowed: true,
         },
         steps: [
