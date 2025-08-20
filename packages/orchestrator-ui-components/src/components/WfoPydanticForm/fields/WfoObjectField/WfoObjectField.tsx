df2e1b569c676f8dfd469aff12605560735c28c0
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-08-20 11:21:40 +0200

    2102: Cleanup

--- packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoObjectField/WfoObjectField.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoObjectField/WfoObjectField.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
     PydanticFormElementProps,
     RenderFields,
+    disableField,
     getPydanticFormComponents,
     usePydanticFormContext,
 } from 'pydantic-forms';
@@ -15,12 +16,22 @@ export const WfoObjectField = ({
     pydanticFormField,
 }: PydanticFormElementProps) => {
     const { config } = usePydanticFormContext();
+    const disabled = pydanticFormField.attributes?.disabled || false;
     const { wfoObjectFieldStyles } = getWfoObjectFieldStyles();
     const components = getPydanticFormComponents(
         pydanticFormField.properties || {},
         config?.componentMatcherExtender,
     );
 
+    // We have decided - for now - on the convention that all descendants of disabled fields will be disabled as well
+    // so we will not displaying any interactive elements inside a disabled element
+    if (disabled) {
+        components.forEach((component) => {
+            component.pydanticFormField = disableField(
+                component.pydanticFormField,
+            );
+        });
+    }
     return (
         <EuiFlexGroup
             data-testid={pydanticFormField.id}
