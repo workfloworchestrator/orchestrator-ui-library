1bac14fa01ee3efb3791bdb1696800d02ac86abd
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-09-29 16:30:47 +0200

    2175: Partially fixes Object object in array items

--- packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import _ from 'lodash';
 import type { PydanticFormControlledElement } from 'pydantic-forms';
+import { getFormFieldIdWithPath } from 'pydantic-forms';
 
 import { EuiFieldNumber } from '@elastic/eui';
 import { css } from '@emotion/react';
@@ -32,13 +34,21 @@ export const WfoInteger: PydanticFormControlledElement = ({
         getFormFieldsBaseStyle,
     );
 
+    // If the field is part of an array the value is passed in as an object with the field name as key
+    // this is imposed by react-hook-form. We try to detect this and extract the actual value
+    const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
+    const fieldValue =
+        _.isObject(value) && _.has(value, fieldName)
+            ? _.get(value, fieldName)
+            : value;
+
     return (
         <EuiFieldNumber
             data-testid={pydanticFormField.id}
             css={formFieldBaseStyle}
             name={pydanticFormField.id}
             onChange={(event) => onChange(parseInt(event.target.value))}
-            value={value}
+            value={fieldValue}
             disabled={disabled}
         />
     );
--- packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoText.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoText.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
+import _ from 'lodash';
 import type { PydanticFormControlledElement } from 'pydantic-forms';
+import { getFormFieldIdWithPath } from 'pydantic-forms';
 
 import { EuiFieldText } from '@elastic/eui';
 
@@ -16,14 +18,20 @@ export const WfoText: PydanticFormControlledElement = ({
     const { formFieldBaseStyle } = useWithOrchestratorTheme(
         getFormFieldsBaseStyle,
     );
-
+    // If the field is part of an array the value is passed in as an object with the field name as key
+    // this is imposed by react-hook-form. We try to detect this and extract the actual value
+    const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
+    const fieldValue =
+        _.isObject(value) && _.has(value, fieldName)
+            ? _.get(value, fieldName)
+            : value;
     return (
         <EuiFieldText
             data-testid={pydanticFormField.id}
             css={formFieldBaseStyle}
             disabled={disabled}
             onChange={(event) => onChange(event.target.value)}
-            value={value ?? ''}
+            value={fieldValue}
             fullWidth
         />
     );
