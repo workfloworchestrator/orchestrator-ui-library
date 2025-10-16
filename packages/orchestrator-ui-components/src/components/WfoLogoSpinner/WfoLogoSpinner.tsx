f67c5bd0649aba1d60182494fa3c9a20a329bcc7
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2025-10-16 14:06:12 +0200

    2198: Wfo loading spinner stub

---
+++ packages/orchestrator-ui-components/src/components/WfoLogoSpinner/WfoLogoSpinner.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+interface WfoLogoSpinnerProps {
+    size?: number;
+}
+
+export const WfoLogoSpinner: FC<WfoLogoSpinnerProps> = () => {
+    return <div>LOGO SPINNER POC</div>;
+};
