7f2ede8cdad39e25fb28345feb8e07dd68055205
Tim Frohlich <timfrohlich@hotmail.com>
2025-10-22 20:40:05 +0200

    remove unused detailmodal

--- packages/orchestrator-ui-components/src/components/WfoSearchPage/WfoSearchResults/WfoSearchResults.tsx
+++ packages/orchestrator-ui-components/src/components/WfoSearchPage/WfoSearchResults/WfoSearchResults.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { EuiFlexGroup, EuiPanel } from '@elastic/eui';
 
@@ -7,7 +7,6 @@ import { SearchResult } from '@/types';
 import { WfoSearchEmptyState } from './WfoSearchEmptyState';
 import { WfoSearchLoadingState } from './WfoSearchLoadingState';
 import { WfoSearchResultItem } from './WfoSearchResultItem';
-import { WfoSubscriptionDetailModal } from './WfoSubscriptionDetailModal';
 
 interface WfoSearchResultsProps {
     results: SearchResult[];
@@ -22,12 +21,6 @@ export const WfoSearchResults = ({
     selectedRecordIndex = 0,
     onRecordSelect,
 }: WfoSearchResultsProps) => {
-    const [modalData, setModalData] = useState<unknown | null>(null);
-
-    const handleCloseModal = () => {
-        setModalData(null);
-    };
-
     if (loading) {
         return <WfoSearchLoadingState />;
     }
@@ -37,27 +30,20 @@ export const WfoSearchResults = ({
     }
 
     return (
-        <>
-            <EuiPanel paddingSize="m" hasShadow={false}>
-                <EuiFlexGroup direction="column" gutterSize="s">
-                    {results.map((result, idx) => (
-                        <WfoSearchResultItem
-                            key={idx}
-                            result={result}
-                            index={idx}
-                            isSelected={idx === selectedRecordIndex}
-                            onSelect={() => {
-                                onRecordSelect?.(idx);
-                            }}
-                        />
-                    ))}
-                </EuiFlexGroup>
-            </EuiPanel>
-            <WfoSubscriptionDetailModal
-                isVisible={!!modalData}
-                onClose={handleCloseModal}
-                resultData={modalData}
-            />
-        </>
+        <EuiPanel paddingSize="m" hasShadow={false}>
+            <EuiFlexGroup direction="column" gutterSize="s">
+                {results.map((result, idx) => (
+                    <WfoSearchResultItem
+                        key={idx}
+                        result={result}
+                        index={idx}
+                        isSelected={idx === selectedRecordIndex}
+                        onSelect={() => {
+                            onRecordSelect?.(idx);
+                        }}
+                    />
+                ))}
+            </EuiFlexGroup>
+        </EuiPanel>
     );
 };
