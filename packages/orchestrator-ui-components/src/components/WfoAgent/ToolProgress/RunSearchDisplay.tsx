61c8550ef60225f8f74fbce9fc58e86128663256
timfdev <35373481+timfdev@users.noreply.github.com>
2025-10-23 20:05:09 +0200

    render exports (#2232)
    * render exports
    
    * update export properties
    
    * Search results are now unified in one model
    
    * update agent results
    
    * Render all tool calls
    
    * remove unused detailmodal
    
    * Update styling for tool calls
    
    * fix lint errors

---
+++ packages/orchestrator-ui-components/src/components/WfoAgent/ToolProgress/RunSearchDisplay.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { EuiFlexGroup, EuiFlexItem, EuiText } from '@elastic/eui';
+
+import { WfoBadge } from '@/components/WfoBadges';
+
+type RunSearchDisplayProps = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    result?: any;
+    parameters: {
+        limit?: number;
+    };
+};
+
+export const RunSearchDisplay = ({ parameters }: RunSearchDisplayProps) => {
+    const { limit = 10 } = parameters;
+
+    return (
+        <div>
+            <EuiFlexGroup gutterSize="s" alignItems="center">
+                <EuiFlexItem grow={false}>
+                    <EuiText size="xs" color="subdued">
+                        <strong>Results Limit</strong>
+                    </EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                    <WfoBadge textColor="default" color="hollow">
+                        {limit}
+                    </WfoBadge>
+                </EuiFlexItem>
+            </EuiFlexGroup>
+        </div>
+    );
+};
