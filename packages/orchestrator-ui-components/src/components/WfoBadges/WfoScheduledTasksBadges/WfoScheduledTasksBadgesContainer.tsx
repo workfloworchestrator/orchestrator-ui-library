c1776c4dc14386add4e2a238f34d23ef844bb42c
Ruben van Leeuwen <ruben.vanleeuwen@surf.nl>
2026-01-19 16:39:49 +0100

    2215: Add schedule column. Fix date

---
+++ packages/orchestrator-ui-components/src/components/WfoBadges/WfoScheduledTasksBadges/WfoScheduledTasksBadgesContainer.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { WfoError } from '@/components/WfoError';
+import { WfoLoading } from '@/components/WfoLoading';
+import { useGetSchedulesForWorkflow } from '@/hooks';
+import { ScheduledTaskDefinition } from '@/types';
+
+import { WfoScheduledTasksBadges } from './WfoScheduledTasksBadges';
+
+export const WfoScheduledTasksBadgesContainer = ({
+    workflowId,
+}: {
+    workflowId: ScheduledTaskDefinition['workflowId'];
+}) => {
+    const { workflowSchedules, isFetching, isError } =
+        useGetSchedulesForWorkflow(workflowId);
+
+    if (isFetching) {
+        return <WfoLoading />;
+    }
+
+    if (isError) {
+        return <WfoError />;
+    }
+
+    return <WfoScheduledTasksBadges workflowSchedules={workflowSchedules} />;
+};
