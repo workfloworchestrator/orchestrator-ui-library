144830fdf5cb604c2a3cb2edf5b47729be8290e9
Georgi2704 <jrmanev@gmail.com>
2026-03-02 16:33:54 +0100

    Fix onChange behaviour for number field

--- packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import _ from 'lodash';
 import type { PydanticFormControlledElement } from 'pydantic-forms';
@@ -11,35 +11,61 @@ import type { WfoThemeHelpers } from '@/hooks';
 import { useWithOrchestratorTheme } from '@/hooks';
 
 const getFormFieldsBaseStyle = ({ theme }: WfoThemeHelpers) => {
-  const formFieldBaseStyle = css({
-    backgroundColor: theme.colors.backgroundBaseSubdued,
-    color: theme.colors.textParagraph,
-    '&:focus': {
-      backgroundColor: theme.colors.backgroundBaseNeutral,
-    },
-  });
+    const formFieldBaseStyle = css({
+        backgroundColor: theme.colors.backgroundBaseSubdued,
+        color: theme.colors.textParagraph,
+        '&:focus': {
+            backgroundColor: theme.colors.backgroundBaseNeutral,
+        },
+    });
 
-  return {
-    formFieldBaseStyle,
-  };
+    return {
+        formFieldBaseStyle,
+    };
 };
 
-export const WfoInteger: PydanticFormControlledElement = ({ pydanticFormField, onChange, value, disabled }) => {
-  const { formFieldBaseStyle } = useWithOrchestratorTheme(getFormFieldsBaseStyle);
+export const WfoInteger: PydanticFormControlledElement = ({
+                                                              pydanticFormField,
+                                                              onChange,
+                                                              value,
+                                                              disabled,
+                                                          }) => {
+    const { formFieldBaseStyle } = useWithOrchestratorTheme(
+        getFormFieldsBaseStyle,
+    );
 
-  // If the field is part of an array the value is passed in as an object with the field name as key
-  // this is imposed by react-hook-form. We try to detect this and extract the actual value
-  const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
-  const fieldValue = _.isObject(value) && _.has(value, fieldName) ? _.get(value, fieldName) : value;
+    // If the field is part of an array the value is passed in as an object with the field name as key
+    // this is imposed by react-hook-form. We try to detect this and extract the actual value
+    const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
+    const fieldValue =
+        _.isObject(value) && _.has(value, fieldName)
+            ? _.get(value, fieldName)
+            : value;
+    const [userInput, setUserInput] = React.useState<string>(
+        fieldValue !== undefined && fieldValue !== null ? String(fieldValue) : '',
+    );
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newValue = event.target.value;
+        if (value === null && newValue !== value) {
+            onChange(parseInt(newValue));
+        }
+        setUserInput(newValue);
+    }
 
-  return (
-    <EuiFieldNumber
-      data-testid={pydanticFormField.id}
-      css={formFieldBaseStyle}
-      name={pydanticFormField.id}
-      onChange={(event) => onChange(event.target.value ? parseInt(event.target.value) : null)}
-      value={fieldValue}
-      disabled={disabled}
-    />
-  );
+    return (
+        <EuiFieldNumber
+            placeholder="Enter a number"
+            data-testid={pydanticFormField.id}
+            css={formFieldBaseStyle}
+            name={pydanticFormField.id}
+            onBlur={(event) =>
+                onChange(
+                    event.target.value ? parseInt(event.target.value) : null,
+                )
+            }
+            onChange={handleOnChange}
+            value={userInput}
+            disabled={disabled}
+        />
+    );
 };
