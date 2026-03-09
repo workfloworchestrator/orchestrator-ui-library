dc1ba23fd0bb40d721fb1315617474ca6491aab6
Ruben van Leeuwen <ruben@sinin.nl>
2026-03-09 14:24:38 +0100

    94: New table type StructedSearchTable WIP

--- packages/orchestrator-ui-components/src/components/WfoTable/index.ts
+++ packages/orchestrator-ui-components/src/components/WfoTable/index.ts
@@ -1,18 +1,14 @@
-export * from './WfoTableSettingsModal';
-
 export * from './utils/columns';
 export * from './utils/constants';
-
 export * from './utils/tableConfigPersistence';
 export * from './utils/tableUtils';
-
+export * from './WfoTableSettingsModal';
 export * from './WfoStatusColorField';
 export * from './WfoTableWithFilter';
-
 export * from './WfoSortButtons';
 export * from './WfoFirstPartUUID';
 export * from './WfoInlineJson';
 export * from './WfoRowContextMenu';
-
 export * from './WfoAdvancedTable';
 export * from './WfoTable';
+export * from './WfoStructuredSearchTable';
