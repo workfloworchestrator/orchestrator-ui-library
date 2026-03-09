50fe414944f70ea8ab7953c7416983ce9e309e66
Ruben van Leeuwen <ruben@sinin.nl>
2026-03-09 14:24:38 +0100

    94: New table type StructedSearchTable WIP

--- packages/orchestrator-ui-components/src/pages/WfoSearchPocPage.tsx
+++ packages/orchestrator-ui-components/src/pages/WfoSearchPocPage.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+// import { QueryBuilder } from 'react-querybuilder';
 
 import { useTranslations } from 'next-intl';
 
-import { EuiSearchBar, EuiSpacer } from '@elastic/eui';
+import { EuiSpacer } from '@elastic/eui';
 
-import { WfoContentHeader, WfoTable } from '@/components';
+import { StoredTableConfig, WfoContentHeader, WfoStructuredSearchTable } from '@/components';
 import type { WfoTableColumnConfig } from '@/components';
 import { ColumnType } from '@/components/WfoTable/WfoTable';
+import { useStoredTableConfig } from '@/hooks';
 import { SearchPayload } from '@/rtk';
 import { useSearchMutation } from '@/rtk';
-import { SearchResult } from '@/types';
+import { type ProductDefinition, SearchResult } from '@/types';
 
 /*
 entity_id: string;
@@ -20,9 +23,21 @@ perfect_match: number;
 matching_field?: MatchingField | null;
 */
 
+const SEARCH_TABLE_LOCAL_STORAGE_KEY = 'SEARCH_TABLE_LOCAL_STORAGE_KEY';
+
 export const WfoSearchPocPage = () => {
   const t = useTranslations('search.page');
   const [triggerSearch, { isLoading, data }] = useSearchMutation();
+  const getStoredTableConfig = useStoredTableConfig<SearchResult>(SEARCH_TABLE_LOCAL_STORAGE_KEY);
+  const [tableDefaults, setTableDefaults] = useState<StoredTableConfig<SearchResult>>();
+
+  useEffect(() => {
+    const storedConfig = getStoredTableConfig();
+    console.log('restored config', storedConfig);
+    if (storedConfig) {
+      setTableDefaults(storedConfig);
+    }
+  }, [getStoredTableConfig]);
 
   const onSearch = (query: unknown, queryText: string) => {
     const searchPayload: SearchPayload = {
@@ -32,7 +47,7 @@ export const WfoSearchPocPage = () => {
     triggerSearch(searchPayload);
   };
 
-  const tableColumns: WfoTableColumnConfig<SearchResult> = {
+  const tableColumnConfig: WfoTableColumnConfig<SearchResult> = {
     entity_id: {
       columnType: ColumnType.DATA,
       label: t('id'),
@@ -68,14 +83,58 @@ export const WfoSearchPocPage = () => {
   return (
     <>
       <WfoContentHeader title="Search page POC" />
-      <EuiSearchBar
-        onChange={(onChangeArgs) => {
-          const { query, queryText } = onChangeArgs;
-          onSearch(query, queryText);
+      <EuiSpacer size="l" />
+      <WfoStructuredSearchTable<SearchResult>
+        data={data?.data || []}
+        isLoading={isLoading}
+        defaultHiddenColumns={tableDefaults?.hiddenColumns}
+        onUpdateQueryString={(queryString) => {
+          console.log('queryString', queryString);
         }}
+        tableColumnConfig={tableColumnConfig}
+        localStorageKey={SEARCH_TABLE_LOCAL_STORAGE_KEY}
       />
-      <EuiSpacer size="xxl" />
-      <WfoTable<SearchResult> data={data?.data ?? []} columnConfig={tableColumns} isLoading={isLoading} />
     </>
   );
 };
+
+/*
+*
+* export type WfoTableProps<T extends object> = {
+  data: T[];
+   // Ommited in parent type columnConfig: WfoTableColumnConfig<T>;
+  hiddenColumns?: TableColumnKeys<T>;
+  columnOrder?: TableColumnKeys<T>;
+  isLoading?: boolean;
+  dataSorting?: WfoDataSorting<T>[];
+  rowExpandingConfiguration?: {
+    uniqueRowId: keyof WfoTableColumnConfig<T>;
+    uniqueRowIdToExpandedRowMap: Record<string, ReactNode>;
+  };
+  pagination?: Pagination;
+  overrideHeader?: (
+    tableHeaderEntries: Array<[string, WfoTableControlColumnConfigItem<T> | WfoTableDataColumnConfigItem<T, keyof T>]>,
+  ) => ReactNode;
+  onRowClick?: (row: T) => void;
+  onUpdateDataSorting?: (updatedDataSorting: WfoDataSorting<T>) => void;
+  onUpdateDataSearch?: (updatedDataSearch: WfoDataSearch<T>) => void;
+  appendFillerColumn?: boolean;
+  className?: string;
+  isVirtualized?: boolean;
+  height?: number;
+};
+*
+*   tableColumnConfig: WfoStructuredSearchTableColumnConfig<T>;
+  defaultHiddenColumns?: TableColumnKeys<T>;
+  queryString?: string;
+  localStorageKey: string;
+  detailModal?: boolean;
+  detailModalTitle?: string;
+  exportDataIsLoading?: boolean;
+  error?: WfoGraphqlError[];
+  onUpdateQueryString: (queryString: string) => void;
+  onExportData?: () => void;
+*
+*
+*
+* */
