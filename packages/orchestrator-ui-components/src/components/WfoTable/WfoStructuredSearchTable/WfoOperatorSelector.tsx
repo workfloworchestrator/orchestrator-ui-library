fbd73fe68d0307d75f3b08cc436d637a8e52c888
Ruben van Leeuwen <ruben@sinin.nl>
2026-03-18 16:45:09 +0100

    94: Set the operators in the operator selector based on the field selected WIP.

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import type { OperatorSelectorProps } from 'react-querybuilder';
+import type { FullOperator, OperatorSelectorProps, OptionGroup } from 'react-querybuilder';
 
-import { EuiSelect, EuiSelectOption } from '@elastic/eui';
+import { EuiSelect } from '@elastic/eui';
+
+import { getOperatorDisplay } from '@/components/WfoSearchPage/utils';
+
+const isOptionGroup = (operator: FullOperator | OptionGroup<FullOperator>): operator is OptionGroup<FullOperator> =>
+  'options' in operator;
 
 export const WfoOperatorSelector = (props: OperatorSelectorProps) => {
-  console.log(props);
-  const options: EuiSelectOption[] = ['+', '-'].map((operator) => ({ text: operator, value: operator }));
-  return <EuiSelect options={options} />;
+  console.log('WfoOperatorSelector props', props);
+  const flatOptions = (props.options as Array<FullOperator | OptionGroup<FullOperator>>).flatMap((option) =>
+    isOptionGroup(option) ? option.options : [option],
+  );
+  const selectOptions = flatOptions.map((option) => {
+    const { symbol, description } = getOperatorDisplay(option.name);
+    return { value: option.name, text: `${symbol} ${description}` };
+  });
+  return (
+    <EuiSelect
+      options={selectOptions}
+      value={props.value}
+      onChange={(e) => props.handleOnChange(e.target.value)}
+      disabled={props.disabled}
+    />
+  );
 };
