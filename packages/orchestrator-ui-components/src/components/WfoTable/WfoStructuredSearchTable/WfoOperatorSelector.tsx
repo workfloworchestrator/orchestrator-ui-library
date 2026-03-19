00cc9ca9743dffe0e7cf478b825fc169fd9962bb
Ruben van Leeuwen <ruben@sinin.nl>
2026-03-19 12:57:18 +0100

    94: Implement custom value editor WIP

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
@@ -3,20 +3,14 @@ import type { FullOperator, OperatorSelectorProps, OptionGroup } from 'react-que
 
 import { EuiSelect } from '@elastic/eui';
 
-import { getOperatorDisplay } from '@/components/WfoSearchPage/utils';
-
 const isOptionGroup = (operator: FullOperator | OptionGroup<FullOperator>): operator is OptionGroup<FullOperator> =>
   'options' in operator;
 
 export const WfoOperatorSelector = (props: OperatorSelectorProps) => {
-  console.log('WfoOperatorSelector props', props);
   const flatOptions = (props.options as Array<FullOperator | OptionGroup<FullOperator>>).flatMap((option) =>
     isOptionGroup(option) ? option.options : [option],
   );
-  const selectOptions = flatOptions.map((option) => {
-    const { symbol, description } = getOperatorDisplay(option.name);
-    return { value: option.name, text: `${symbol} ${description}` };
-  });
+  const selectOptions = flatOptions.map((option) => ({ value: option.name, text: option.label }));
   return (
     <EuiSelect
       options={selectOptions}
