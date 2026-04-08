cb86cd20abcea27b035ea4a99a6b229b29796e99
Georgi2704 <jrmanev@gmail.com>
2026-04-08 09:48:21 +0200

    Update pydantic-forms to 1.0.7

--- packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
+++ packages/orchestrator-ui-components/src/components/WfoPydanticForm/fields/WfoInteger.tsx
@@ -11,68 +11,54 @@ import type { WfoThemeHelpers } from '@/hooks';
 import { useWithOrchestratorTheme } from '@/hooks';
 
 const getFormFieldsBaseStyle = ({ theme }: WfoThemeHelpers) => {
-    const formFieldBaseStyle = css({
-        backgroundColor: theme.colors.backgroundBaseSubdued,
-        color: theme.colors.textParagraph,
-        '&:focus': {
-            backgroundColor: theme.colors.backgroundBaseNeutral,
-        },
-    });
+  const formFieldBaseStyle = css({
+    backgroundColor: theme.colors.backgroundBaseSubdued,
+    color: theme.colors.textParagraph,
+    '&:focus': {
+      backgroundColor: theme.colors.backgroundBaseNeutral,
+    },
+  });
 
-    return {
-        formFieldBaseStyle,
-    };
+  return {
+    formFieldBaseStyle,
+  };
 };
 
-export const WfoInteger: PydanticFormControlledElement = ({
-    pydanticFormField,
-    onChange,
-    value,
-    disabled,
-}) => {
-    const { formFieldBaseStyle } = useWithOrchestratorTheme(
-        getFormFieldsBaseStyle,
-    );
+export const WfoInteger: PydanticFormControlledElement = ({ pydanticFormField, onChange, value, disabled }) => {
+  const { formFieldBaseStyle } = useWithOrchestratorTheme(getFormFieldsBaseStyle);
 
-    // If the field is part of an array the value is passed in as an object with the field name as key
-    // this is imposed by react-hook-form. We try to detect this and extract the actual value
-    const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
-    const fieldValue =
-        _.isObject(value) && _.has(value, fieldName)
-            ? _.get(value, fieldName)
-            : value;
-    const [userInput, setUserInput] = React.useState<string>();
+  // If the field is part of an array the value is passed in as an object with the field name as key
+  // this is imposed by react-hook-form. We try to detect this and extract the actual value
+  const fieldName = getFormFieldIdWithPath(pydanticFormField.id);
+  const fieldValue = _.isObject(value) && _.has(value, fieldName) ? _.get(value, fieldName) : value;
+  const [userInput, setUserInput] = React.useState<string>();
 
-    useEffect(() => {
-        if (fieldValue !== undefined && fieldValue !== null) {
-            setUserInput(fieldValue);
-        } else {
-            setUserInput('');
-        }
-    }, [fieldValue, value]);
+  useEffect(() => {
+    if (fieldValue !== undefined && fieldValue !== null) {
+      setUserInput(fieldValue);
+    } else {
+      setUserInput('');
+    }
+  }, [fieldValue, value]);
 
-    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const newValue = event.target.value;
-        if (value === null && newValue !== value) {
-            onChange(parseInt(newValue));
-        }
-        setUserInput(newValue);
-    };
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = event.target.value;
+    if (value === null && newValue !== value) {
+      onChange(parseInt(newValue));
+    }
+    setUserInput(newValue);
+  };
 
-    return (
-        <EuiFieldNumber
-            placeholder="Enter a number"
-            data-testid={pydanticFormField.id}
-            css={formFieldBaseStyle}
-            name={pydanticFormField.id}
-            onBlur={(event) =>
-                onChange(
-                    event.target.value ? parseInt(event.target.value) : null,
-                )
-            }
-            onChange={handleOnChange}
-            value={userInput}
-            disabled={disabled}
-        />
-    );
+  return (
+    <EuiFieldNumber
+      placeholder="Enter a number"
+      data-testid={pydanticFormField.id}
+      css={formFieldBaseStyle}
+      name={pydanticFormField.id}
+      onBlur={(event) => onChange(event.target.value ? parseInt(event.target.value) : null)}
+      onChange={handleOnChange}
+      value={userInput}
+      disabled={disabled}
+    />
+  );
 };
