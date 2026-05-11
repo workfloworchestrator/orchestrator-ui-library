039df13651a695b4faea4bd30ace9d8c5ee58eae
Ruben van Leeuwen <ruben@sinin.nl>
2026-05-11 13:33:26 +0200

    Fix typing of result data

--- packages/orchestrator-ui-components/src/components/WfoSearchPage/constants.ts
+++ packages/orchestrator-ui-components/src/components/WfoSearchPage/constants.ts
@@ -6,7 +6,3 @@ export const LAYOUT_RATIOS = {
   RESULTS_GROW: 2,
   DETAIL_GROW: 3,
 } as const;
-
-export const VALID_ENTITY_TYPES = ['SUBSCRIPTION', 'PRODUCT', 'WORKFLOW', 'PROCESS'] as const;
-
-export const DEFAULT_ENTITY_TAB = 'SUBSCRIPTION';
