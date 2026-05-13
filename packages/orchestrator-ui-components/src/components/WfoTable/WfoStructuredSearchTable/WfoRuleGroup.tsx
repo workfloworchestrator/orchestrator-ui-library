256cc1bffa7ac6695a882fd69d71612175726ddf
Ruben van Leeuwen <ruben@sinin.nl>
2026-05-13 15:29:50 +0200

    94: Remove rule button stlying

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoRuleGroup.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoRuleGroup.tsx
@@ -4,11 +4,14 @@ import { RuleGroupBodyComponents, RuleGroupHeaderComponents, useRuleGroup } from
 
 import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 
+import { getWfoStructuredSearchTableStyles } from '@/components/WfoTable/WfoStructuredSearchTable/styles';
+import { useWithOrchestratorTheme } from '@/hooks';
+
 export const WfoRuleGroup = (props: RuleGroupProps) => {
   const ruleGroupProps = useRuleGroup(props);
-
+  const { ruleGroupContainerStyles } = useWithOrchestratorTheme(getWfoStructuredSearchTableStyles);
   return (
-    <EuiFlexGroup direction="column" gutterSize="s">
+    <EuiFlexGroup direction="column" gutterSize="s" css={ruleGroupContainerStyles}>
       <EuiFlexItem>
         <EuiFlexGroup gutterSize="none">
           <RuleGroupHeaderComponents {...ruleGroupProps} />
