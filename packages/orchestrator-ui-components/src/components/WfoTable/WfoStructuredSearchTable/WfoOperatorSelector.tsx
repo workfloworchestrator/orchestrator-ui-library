337424c7744b0d54b068dea93f8eb21448478f26
Ruben van Leeuwen <ruben@sinin.nl>
2026-05-13 15:56:14 +0200

    94: Select a default value in the operator selector

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoOperatorSelector.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
 import type { FullOperator, OperatorSelectorProps, OptionGroup } from 'react-querybuilder';
 
 import { EuiSelect } from '@elastic/eui';
@@ -10,7 +10,17 @@ export const WfoOperatorSelector = (props: OperatorSelectorProps) => {
   const flatOptions = (props.options as Array<FullOperator | OptionGroup<FullOperator>>).flatMap((option) =>
     isOptionGroup(option) ? option.options : [option],
   );
-  const selectOptions = flatOptions.map((option) => ({ value: option.name, text: option.label }));
+  const selectOptions = useMemo(
+    () => flatOptions.map((option) => ({ value: option.name, text: option.label })),
+    [flatOptions],
+  );
+
+  useEffect(() => {
+    if (props.options && !props.value && selectOptions.length > 0) {
+      props.handleOnChange(selectOptions[0].value);
+    }
+  }, [props, props.options, props.value, selectOptions]);
+
   return (
     <EuiSelect
       options={selectOptions}
