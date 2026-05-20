f5bedbb80536cef3f7960483faeeaea0c68a3367
Ruben van Leeuwen <ruben@sinin.nl>
2026-05-20 11:30:24 +0200

    94: Adds date picker component WIP

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoValueEditor.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoValueEditor.tsx
@@ -2,7 +2,10 @@ import React, { type KeyboardEventHandler, useEffect, useState } from 'react';
 import type { ChangeEventHandler } from 'react';
 import type { ValueEditorProps } from 'react-querybuilder';
 
-import { EuiButtonGroup, EuiFieldText } from '@elastic/eui';
+import moment from 'moment';
+import { useTranslations } from 'next-intl';
+
+import { EuiButtonGroup, EuiDatePicker, EuiFieldText } from '@elastic/eui';
 
 import { getWfoStructuredSearchTableStyles } from '@/components/WfoTable/WfoStructuredSearchTable/styles';
 import { useWithOrchestratorTheme } from '@/hooks';
@@ -11,15 +14,16 @@ enum UiFieldType {
   text = 'text',
   number = 'number',
   boolean = 'boolean',
+  datetime = 'datetime',
 }
 
-const BooleanToggle = ({
-  handleOnChange,
-  currentValue,
-}: {
+interface EditorProps<T = boolean> {
   handleOnChange: (value: unknown) => void;
-  currentValue?: boolean;
-}) => {
+  currentValue?: T;
+  operator?: string;
+}
+
+const BooleanEditor = ({ handleOnChange, currentValue }: EditorProps<boolean>) => {
   const [value, setValue] = useState<string>('true');
 
   useEffect(() => {
@@ -57,24 +61,15 @@ const BooleanToggle = ({
   );
 };
 
-export const WfoValueEditor = (props: ValueEditorProps) => {
-  const [value, setValue] = useState<string>(props.value);
-  const fieldPathInfoMap = props.context?.fieldPathInfoMap;
-  const fieldName = props.field;
-  const fieldInfo = fieldPathInfoMap && fieldPathInfoMap.has(fieldName) ? fieldPathInfoMap.get(fieldName) : undefined;
+const TextEditor = ({ handleOnChange, currentValue }: EditorProps<string>) => {
+  const [value, setValue] = useState<string>(currentValue || '');
 
-  const uiFieldType = fieldInfo?.ui_types[0] || UiFieldType.text;
-
-  if (uiFieldType === UiFieldType.boolean) {
-    return <BooleanToggle handleOnChange={props.handleOnChange} currentValue={props.value as boolean} />;
-  }
-
-  const handleOnChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  const handleTextChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setValue(e.target.value || '');
   };
 
   const handleOnBlur = () => {
-    props.handleOnChange(value);
+    handleOnChange(value);
   };
 
   const handleOnKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
@@ -83,5 +78,76 @@ export const WfoValueEditor = (props: ValueEditorProps) => {
     }
   };
 
-  return <EuiFieldText value={value} onChange={handleOnChange} onBlur={handleOnBlur} onKeyDown={handleOnKeyDown} />;
+  return <EuiFieldText value={value} onChange={handleTextChange} onBlur={handleOnBlur} onKeyDown={handleOnKeyDown} />;
+};
+
+interface DatePickerProps {
+  value: string;
+  handleOnChange: (value: string | undefined) => void;
+  // rangeIndex: number;
+}
+const DatePicker = ({ value, handleOnChange }: DatePickerProps) => {
+  const [date, setDate] = useState<string | undefined>(value);
+  const t = useTranslations('search.page');
+
+  return (
+    <EuiDatePicker
+      selected={date ? moment.utc(date) : null}
+      onChange={(date) => {
+        const utcDate = date ? moment.utc(date) : null;
+        setDate(utcDate?.toISOString() || undefined);
+        handleOnChange(utcDate?.toISOString() || undefined);
+      }}
+      css={{ width: '330px' }}
+      showTimeSelect
+      dateFormat="yyyy-MM-dd HH:mm"
+      timeFormat="HH:mm"
+      placeholderText={t('selectDateAndTime')}
+      locale="nl"
+    />
+  );
+};
+
+/*
+const DatetimeEditor = ({ currentValue, handleOnChange, operator }: EditorProps<string>) => {
+  const [value, setValue] = useState<[string|undefined][]>(currentValue || []);
+
+  const onChange = (value: string, rangeIndex: number = 0) => {
+    if (operator === 'between') {
+      setValue((currentValue) => {
+
+      });
+    } else {
+      setValue(value);
+      handleOnChange(value);
+    }
+  };
+
+  if (operator === 'between') {
+    return (
+      <div>
+        <DatePicker value={value} onChange={onChange} rangeIndex={0} />;
+        <DatePicker value={value} onChange={onChange} rangeIndex={1} />;
+      </div>
+    );
+  }
+
+  return <DatePicker value={value} onChange={onChange} rangeIndex={0} />;
+};
+*/
+export const WfoValueEditor = ({ field: fieldName, context, handleOnChange, value }: ValueEditorProps) => {
+  const fieldPathInfoMap = context?.fieldPathInfoMap;
+
+  const fieldInfo = fieldPathInfoMap && fieldPathInfoMap.has(fieldName) ? fieldPathInfoMap.get(fieldName) : undefined;
+  const uiFieldType = fieldInfo?.ui_types[0] || UiFieldType.text;
+
+  if (uiFieldType === UiFieldType.boolean) {
+    return <BooleanEditor handleOnChange={handleOnChange} />;
+  }
+
+  if (uiFieldType === UiFieldType.datetime) {
+    return <DatePicker handleOnChange={handleOnChange} value={value as string} />;
+  }
+
+  return <TextEditor handleOnChange={handleOnChange} />;
 };
