fcc286cbcc7d7398918e594f03bc94c047b66ddc
Ruben van Leeuwen <ruben@sinin.nl>
2026-06-02 14:09:41 +0200

    116: Create expandingSearchRow component

--- packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoExpandingSearchRow.tsx
+++ packages/orchestrator-ui-components/src/components/WfoTable/WfoStructuredSearchTable/WfoExpandingSearchRow.tsx
@@ -1,1 +1,18 @@
-WfoExpadingSearchRow.tsx;
+import React from 'react';
+
+import type { MatchingField } from '@/types';
+
+interface WfoExpandingSearchRowProps {
+  score?: number;
+  perfectMatch?: number;
+  matchingField?: MatchingField | null;
+}
+
+export const WfoExpandingSearchRow = ({ score, perfectMatch, matchingField }: WfoExpandingSearchRowProps) => {
+  console.log(score, perfectMatch, matchingField);
+  return (
+    <div>
+      {score} - {perfectMatch}
+    </div>
+  );
+};
